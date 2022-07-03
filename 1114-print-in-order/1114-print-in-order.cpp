e359c1fe77b7ea53cfb642d1e06db1980eada313
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2022-07-02 19:11:20 -0700

    Time: 78 ms (44.61%), Space: 7.3 MB (17.47%) - LeetHub

---
+++ 1114-print-in-order/1114-print-in-order.cpp
@@ -0,0 +1,32 @@
+#include <semaphore.h>
+
+class Foo {
+
+protected:
+    sem_t firstJobDone;
+    sem_t secondJobDone;
+
+public:
+
+    Foo() {
+        sem_init(&firstJobDone, 0, 0);
+        sem_init(&secondJobDone, 0, 0);
+    }
+
+    void first(function<void()> printFirst) {
+        printFirst();
+        sem_post(&firstJobDone);
+    }
+
+    void second(function<void()> printSecond) {
+        sem_wait(&firstJobDone);
+        printSecond();
+        sem_post(&secondJobDone);
+        
+    }
+
+    void third(function<void()> printThird) {
+        sem_wait(&secondJobDone);
+        printThird();
+    }
+};
