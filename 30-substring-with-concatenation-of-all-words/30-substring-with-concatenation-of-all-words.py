1419eec01fa7bec5f5194d7416aab6d895c457e4
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2022-08-12 18:08:17 -0700

    Time: 462 ms (66.67%), Space: 14.3 MB (38.75%) - LeetHub

---
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        n = len(s)
+        k = len(words)
+        word_length = len(words[0])
+        substring_size = word_length * k
+        word_count = collections.Counter(words)
+        
+        def check(i):
+            remaining = word_count.copy()
+            words_used = 0
+
+            for j in range(i, i + substring_size, word_length):
+                sub = s[j : j + word_length]
+                if remaining[sub] > 0:
+                    remaining[sub] -= 1
+                    words_used += 1
+                else:
+                    break
+            
+            return words_used == k
+        
+        answer = []
+        for i in range(n - substring_size + 1):
+            if check(i):
+                answer.append(i)
+
+        return answer
