9ac9a582ee007759329f4adc659b1842560fb38e
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2022-10-19 23:15:33 -0700

    Time: 90 ms (28.12%), Space: 13.2 MB (87.76%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,5 +1,9 @@
-class Solution:
-    def intToRoman(self, num: int) -> str:
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
         digits = [(1000, "M"), (900, "CM"), (500, "D"), (400, "CD"), (100, "C"), 
                   (90, "XC"), (50, "L"), (40, "XL"), (10, "X"), (9, "IX"), 
                   (5, "V"), (4, "IV"), (1, "I")]
