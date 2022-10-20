8d03c394da884c50e72f7e6008f8cd58dad0accd
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2022-10-19 23:14:27 -0700

    Time: 21 ms (34.27%), Space: 5.8 MB (92.11%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,15 +1,15 @@
 class Solution {
-    private static final int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};    
-    private static final String[] symbols = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
-
-    public String intToRoman(int num) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < values.length && num > 0; i++) {
+public:
+    string intToRoman(int num) {
+        const int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};    
+        const string symbols[] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"}; 
+        string s;
+        for (int i = 0; i < 13; i++) {
             while (values[i] <= num) {
                 num -= values[i];
-                sb.append(symbols[i]);
+                s.append(symbols[i]);
             }
         }
-        return sb.toString();
+        return s;
     }
-}
+};
