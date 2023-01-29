79407fddba7bdabbc873f6fd3e76761d1ac8c835
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-01-29 12:33:10 -0800

    Time: 7 ms (92.02%), Space: 6.2 MB (32.62%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.c
@@ -0,0 +1,21 @@
+char * longestPalindrome(char * s){
+    int n = strlen(s);
+    if (n <= 1) return s;
+    int start = 0, len = 1;
+    for (int i = 0; i < n; ) {
+        int j = i, k = i;
+        while (k < n - 1 && s[k + 1] == s[k]) ++k; // skip duplicate characters
+        i = k + 1;
+        while (j > 0 && k < n - 1 && s[j - 1] == s[k + 1]) { // expand from center
+            --j;
+            ++k;
+        }
+        int newLen = k - j + 1;
+        if (newLen > len) {
+            start = j;
+            len = newLen;
+        }
+    }
+    s[start + len] = '\0';
+    return s + start;
+}
