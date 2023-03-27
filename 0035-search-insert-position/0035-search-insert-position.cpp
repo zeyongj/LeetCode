3e37110024fb9dfe20c1b8dc097ef2fae999be5f
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-03-27 14:48:33 -0700

    Time: 3 ms (88.98%), Space: 9.5 MB (97.09%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int pivot = 0;
-        int left = 0;
-        int right = nums.size() - 1;
+        int pivot, left = 0, right = nums.size() - 1;
         while (left <= right) {
-          pivot = left + (right - left) / 2;
-          if (nums[pivot] == target) 
-              return pivot;
-          if (target < nums[pivot]) 
-              right = pivot - 1;
-          else 
-              left = pivot + 1;
-        }
+            pivot = left + (right - left) / 2;
+            if (nums[pivot] == target) return pivot;
+            if (target < nums[pivot]) right = pivot - 1;
+            else left = pivot + 1;
+    }
         return left;
     }
 };
