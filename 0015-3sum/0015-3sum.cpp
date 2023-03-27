dc8a685891fc975bd0c4a637de1adb0de382d685
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-03-27 14:42:11 -0700

    Time: 149 ms (79.31%), Space: 24.1 MB (64.77%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    std::vector<std::vector<int>> threeSum(std::vector<int>& nums) {
+        std::vector<std::vector<int>> result;
+        if (nums.size() < 3) return result;
+
+        std::sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < nums.size() - 2; ++i) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1;
+            int right = nums.size() - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum < 0) {
+                    left++;
+                } else if (sum > 0) {
+                    right--;
+                } else {
+                    result.push_back({nums[i], nums[left], nums[right]});
+                    left++;
+                    right--;
+
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+                }
+            }
+        }
+
+        return result;
+    }
+};
