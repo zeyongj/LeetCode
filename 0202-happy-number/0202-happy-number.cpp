f5d9cbcde51604e65fba9c11a2a1830dbf3a7a1f
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-04-09 16:49:38 -0700

    Time: 2 ms (57.58%), Space: 6 MB (54.53%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n = n / 10;
+            totalSum += d * d;
+        }
+        return totalSum;
+    }
+    
+    bool isHappy(int n) {
+        while (n != 1 && n != 4) {
+            n = getNext(n);
+        }
+        return n == 1;
+    }
+};
