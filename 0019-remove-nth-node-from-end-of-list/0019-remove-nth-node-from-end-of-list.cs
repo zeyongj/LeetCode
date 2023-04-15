88deb13f18e0c0c42f52fc2115062ea46060a3ba
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-04-15 15:04:49 -0700

    Time: 87 ms (54.88%), Space: 38.7 MB (66.14%) - LeetHub

---
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cs
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode RemoveNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode first = dummy;
+        ListNode second = dummy;
+
+        // Move first pointer n nodes ahead in the linked list
+        for (int i = 0; i < n + 1; i++) {
+            first = first.next;
+        }
+
+        // Move first to the end, maintaining the gap
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+
+        // Skip the desired node
+        second.next = second.next.next;
+
+        // Return the new head node
+        return dummy.next;
+    }
+}
