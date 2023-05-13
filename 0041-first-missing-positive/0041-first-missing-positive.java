787c7b8acfc83d57b7773593676cf61a5ed91038
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-05-12 22:51:25 -0700

    Time: 1 ms (100.00%), Space: 50.7 MB (82.32%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+
+        // First, segregate positive numbers and non-positive numbers.
+        for (int i = 0; i < n; ++i) {
+            while(nums[i] > 0 && nums[i] <= n) {
+                int correctPos = nums[i] - 1; // Calculate correct position first
+                if(nums[correctPos] != nums[i]) {
+                    int temp = nums[i];
+                    nums[i] = nums[correctPos];
+                    nums[correctPos] = temp;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        // Then, find the first index where the index+1 doesn't match the number at that index
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] != i + 1) {
+                return i + 1;  // As the array is 0-indexed, the first missing positive is i+1
+            }
+        }
+
+        // If all indices matched, then the first missing positive is the array size+1
+        return n + 1;
+    }
+}
