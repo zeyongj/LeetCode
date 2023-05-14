7d87aa64a11709cdad58ce9ac83e0c8175d7f933
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-05-14 12:03:25 -0700

    Time: 29 ms (75.33%), Space: 6.4 MB (47.32%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+    }
+
+private:
+    bool solve(vector<vector<char>>& board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    for (char c = '1'; c <= '9'; c++) {
+                        if (isValid(board, i, j, c)) {
+                            board[i][j] = c; // place number
+
+                            if (solve(board)) {
+                                return true; // solve the rest of the grid
+                            }
+
+                            board[i][j] = '.'; // undo number placement, backtrack
+                        }
+                    }
+                    return false; // no valid numbers can be placed in this cell
+                }
+            }
+        }
+        return true; // all cells are filled
+    }
+
+    bool isValid(vector<vector<char>>& board, int row, int col, char c) {
+        for (int i = 0; i < 9; i++) {
+            // check row
+            if (board[i][col] == c) {
+                return false;
+            }
+            // check column
+            if (board[row][i] == c) {
+                return false;
+            }
+            // check 3x3 box
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
