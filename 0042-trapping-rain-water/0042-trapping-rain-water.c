0393f7e47f85b8b4319c1aaae3c542e47057f8e2
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-05-16 13:02:04 -0700

    Time: 16 ms (41.90%), Space: 7.2 MB (17.82%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.c
@@ -0,0 +1,25 @@
+int trap(int* height, int heightSize){
+    int left = 0, right = heightSize - 1;
+    int left_max = 0, right_max = 0;
+    int total_water = 0;
+
+    while (left < right) {
+        if (height[left] < height[right]) {
+            if (height[left] > left_max) {
+                left_max = height[left];
+            } else {
+                total_water += left_max - height[left];
+            }
+            left++;
+        } else {
+            if (height[right] > right_max) {
+                right_max = height[right];
+            } else {
+                total_water += right_max - height[right];
+            }
+            right--;
+        }
+    }
+    return total_water;
+
+}
