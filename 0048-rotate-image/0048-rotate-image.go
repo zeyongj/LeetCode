b8f04373bde95231786dbdba1a79f5aedb19bee0
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-05-30 11:19:06 -0700

    Time: 1 ms (67.01%), Space: 2.2 MB (70.33%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.go
@@ -0,0 +1,17 @@
+func rotate(matrix [][]int)  {
+    n := len(matrix)
+    
+    // Transpose the matrix
+    for i := 0; i < n; i++ {
+        for j := i; j < n; j++ {
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        }
+    }
+    
+    // Reverse each row
+    for i := 0; i < n; i++ {
+        for j := 0; j < n/2; j++ {
+            matrix[i][j], matrix[i][n-j-1] = matrix[i][n-j-1], matrix[i][j]
+        }
+    }
+}
