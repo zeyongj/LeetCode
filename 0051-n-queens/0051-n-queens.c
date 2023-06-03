2bc35da4716b43b089cf1d69d54a450d1cb761ce
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-06-03 14:09:47 -0700

    Time: 7 ms (56.34%), Space: 6.9 MB (98.59%) - LeetHub

--- 0051-n-queens/0051-n-queens.c
+++ 0051-n-queens/0051-n-queens.c
@@ -1,29 +1,84 @@
-class Solution:
-    def solveNQueens(self, n: int):
-        def could_place(row, col):
-            for i in range(row):
-                if board[i] == col or \
-                    board[i] - i == col - row or \
-                    board[i] + i == col + row:
-                    return False
-            return True
-        
-        def place_queen(row, n):
-            for col in range(n):
-                if could_place(row, col):
-                    board[row] = col
-                    if row + 1 == n:
-                        result.append(board[:])
-                    else:
-                        place_queen(row + 1, n)
-        
-        def to_result(board):
-            res = []
-            for i in board:
-                res.append("." * i + "Q" + "." * (n - 1 - i))
-            return res
+void mark(int n, int** checkerboard, int i, int j){
+    int left = j;
+    int right = j;
+    i++;
+    right++;
+    left--;
+    while (i < n){
+        if (right < n){
+            checkerboard[i][right]++;
+            right++;
+        }
+        if (left >= 0){
+            checkerboard[i][left]++;
+            left--;
+        }
+        checkerboard[i][j]++;
+        i++;
+    }
+    return;
+}
 
-        result = []
-        board = [-1] * n
-        place_queen(0, n)
-        return [to_result(sol) for sol in result]
+void del_mark(int n, int** checkerboard, int i, int j){
+    int left = j;
+    int right = j;
+    i++;
+    right++;
+    left--;
+    while (i < n){
+        if (right < n){
+            checkerboard[i][right]--;
+            right++;
+        }
+        if (left >= 0){
+            checkerboard[i][left]--;
+            left--;
+        }
+        checkerboard[i][j]--;
+        i++;
+    }
+    return;
+}
+
+void check(int** checkerboard, int n, int* returnSize, int i, char*** ans, int** returnColumnSizes, int* location){
+    if (i == n){
+        ans[*returnSize] = malloc(sizeof(char*)*n);
+        for (int k = 0 ; k < n ; k++){
+            ans[*returnSize][k] = malloc(sizeof(char)*(n+1));
+            for (int t = 0 ; t < n ; t++){
+                if (t == location[k]){
+                    ans[*returnSize][k][t] = 'Q';
+                } else ans[*returnSize][k][t] = '.';
+            }
+            ans[*returnSize][k][n] = '\0';
+        }
+        (*returnColumnSizes)[*returnSize] = n;
+        *returnSize += 1;
+        return;
+    }
+    for (int j = 0 ; j < n ; j++){
+        if (checkerboard[i][j] == 0){
+            mark( n, checkerboard, i, j);
+            location[i] = j;
+            check(checkerboard, n, returnSize, i+1, ans, returnColumnSizes, location);
+            del_mark( n, checkerboard, i, j);
+        } 
+    }
+}
+
+char *** solveNQueens(int n, int* returnSize, int** returnColumnSizes){
+    char*** ans = malloc(sizeof(char**)*352);
+    (*returnColumnSizes) = malloc(sizeof(int)*352);
+    int location[10];
+    int** checkerboard = malloc(sizeof(int*)*n);
+    for (int i = 0 ; i < n ; i++){
+        checkerboard[i] = calloc(n,sizeof(int));
+    }
+    *returnSize = 0;
+    check(checkerboard, n, returnSize, 0, ans, returnColumnSizes, location);
+    for (int i = 0 ; i < n ; i++){
+        free(checkerboard[i]);
+    }
+    free(checkerboard);
+    return ans;
+}
