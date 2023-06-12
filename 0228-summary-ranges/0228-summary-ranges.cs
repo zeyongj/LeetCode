9732c992c0a4b73f92c25215778d100eb36b2d6e
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-06-12 15:00:58 -0700

    Time: 154 ms (59.85%), Space: 43.4 MB (49.27%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.cs
+++ 0228-summary-ranges/0228-summary-ranges.cs
@@ -1,32 +1,20 @@
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
-char** summaryRanges(int* nums, int numsSize, int* returnSize) {
-	*returnSize=0;
-	if(0==numsSize){
-		return 0;
-	}
-	char **ret=malloc(sizeof(char*)*numsSize);
-	char buff[265];
-	int p1=0;
-	int p2=1;
-	while(p1<numsSize){
-		while(p2<numsSize && nums[p2-1]==nums[p2]-1) ++p2;
-
-		if(p1 < p2-1){
-			int isp=sprintf(buff,"%d->%d",nums[p1],nums[p2-1]);
-			char *m=malloc(sizeof(char)*(isp+1));
-			ret[*returnSize]=strcpy(m,buff);
-			++(*returnSize);
-		}else{
-			int isp=sprintf(buff,"%d",nums[p1]);
-			printf("n:%d,isp:%d\n",nums[p1],isp);
-			char *m=malloc(sizeof(char)*(isp+1));
-			ret[*returnSize]=strcpy(m,buff);
-			++(*returnSize);
-		}
-		p1=p2;
-		++p2;
-	}
-	return ret;
+public class Solution {
+    public IList<string> SummaryRanges(int[] nums) {
+        if (nums == null || nums.Length == 0)
+            return new List<string>();
+        
+        List<string> res = new List<string>();
+        int i = 0;
+        
+        for (int j = 0; j < nums.Length - 1; j++)
+            if (nums[j] + 1 != nums[j + 1])
+            {
+                res.Add(nums[i] == nums[j] ? nums[i].ToString() : nums[i].ToString() + "->" + nums[j].ToString());
+                i = j + 1;
+            }
+        
+        res.Add(i == nums.Length - 1 ? nums[i].ToString() : nums[i].ToString() + "->" + nums[nums.Length - 1].ToString());
+        
+        return res;
+    }
 }
