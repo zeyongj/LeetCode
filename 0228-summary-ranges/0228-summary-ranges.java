be038feb9bdb09f68d31e42269559c59982888f2
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-06-12 14:57:44 -0700

    Time: 5 ms (84.18%), Space: 40.9 MB (57.57%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> result = new ArrayList<>();
+        if (nums.length == 0) {
+            return result;
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            int start = nums[i];
+            
+            while (i + 1 < nums.length && nums[i] + 1 == nums[i + 1]) {
+                i++;
+            }
+            
+            if (start != nums[i]) {
+                result.add(start + "->" + nums[i]);
+            } else {
+                result.add(String.valueOf(start));
+            }
+        }
+        
+        return result;
+    }
+}
