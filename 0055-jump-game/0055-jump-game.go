59a9a59fcceebbd91f5efd10d2fc45c80b85037c
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-06-16 15:22:08 -0700

    Time: 61 ms (79.90%), Space: 7.1 MB (98.48%) - LeetHub

--- 0055-jump-game/0055-jump-game.go
+++ 0055-jump-game/0055-jump-game.go
@@ -1,14 +1,12 @@
-/**
- * @param {number[]} nums
- * @return {boolean}
- */
-var canJump = function(nums) {
-        let len = nums.length;
-        let lastGoodIndex = len - 1;
-        for (let i = len - 1; i >= 0; i--) {
-            if (i + nums[i] >= lastGoodIndex) {
-                lastGoodIndex = i;
-            }
-        }
-        return lastGoodIndex == 0;            
-};
+func canJump(nums []int) bool {
+	reachable := 0
+	for i, n := range nums {
+		if reachable < i {
+			return false
+		}
+		if i+n > reachable {
+			reachable = i + n
+		}
+	}
+	return true  
+}
