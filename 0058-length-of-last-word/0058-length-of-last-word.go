7e4ff0109ab4aa008ba9c37e47336fcac558530d
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-06-20 15:27:35 -0700

    Time: 0 ms (100.00%), Space: 2.1 MB (33.36%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.go
+++ 0058-length-of-last-word/0058-length-of-last-word.go
@@ -1,7 +1,7 @@
-/**
- * @param {string} s
- * @return {number}
- */
-var lengthOfLastWord = function(s) {    
-    return s.trimEnd().split(" ").pop().length
-};
+import "strings"
+
+func lengthOfLastWord(s string) int {
+	words := strings.Fields(s)
+	lastWord := words[len(words)-1]
+	return len(lastWord)
+}
