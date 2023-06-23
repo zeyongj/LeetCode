fb5f76bbd51084785b104c49a490b71c5cfb15ca
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-06-23 13:01:41 -0700

    Time: 169 ms (14.81%), Space: 46.9 MB (10.19%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cs
+++ 0057-insert-interval/0057-insert-interval.cs
@@ -1,52 +1,19 @@
-/**
- * Return an array of arrays of size *returnSize.
- * The sizes of the arrays are returned as *returnColumnSizes array.
- * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
- */
-int** insert(int** intervals, int intervalsSize, int* intervalsColSize, int* newInterval, int newIntervalSize, int* returnSize, int** returnColumnSizes){
-  *returnSize = 0;
-  int head = newInterval[0], end = newInterval[1], idx, inserted = 0;
-  int **result = (int**)malloc(sizeof(int*) * (intervalsSize + 1));
-  *returnColumnSizes = (int*)malloc(sizeof(int) * (intervalsSize + 1));
-  for(idx = 0; idx < intervalsSize; idx++){   
-    if (head > intervals[idx][1]){
-      result[*returnSize] = (int*)malloc(sizeof(int) * 2);
-      result[*returnSize][0] = intervals[idx][0];
-      result[*returnSize][1] = intervals[idx][1];
-      (*returnColumnSizes)[*returnSize] = 2;
-      (*returnSize)++;
+public class Solution {
+    public int[][] Insert(int[][] intervals, int[] newInterval) {
+        List<int[]> res = new List<int[]>();
+        int i = 0;
+        
+        for (;i < intervals.Length && intervals[i][1] < newInterval[0]; i++)
+            res.Add(intervals[i]);
+        
+        for (;i < intervals.Length && newInterval[0] <= intervals[i][1] && newInterval[1] >= intervals[i][0]; i++)
+            newInterval = new int[] { Math.Min(intervals[i][0], newInterval[0]), Math.Max(intervals[i][1], newInterval[1]) };
+        
+        res.Add(newInterval);
+        
+        for (;i < intervals.Length; i++)
+                res.Add(intervals[i]);
+        
+        return res.ToArray();
     }
-    else{ 
-      if (head > intervals[idx][0]) head = intervals[idx][0];      
-      if (end < intervals[idx][0]){        
-        result[*returnSize] = (int*)malloc(sizeof(int) * 2);
-        result[*returnSize][0] = head;
-        result[*returnSize][1] = end;
-        (*returnColumnSizes)[*returnSize] = 2;
-        (*returnSize)++;
-        inserted = 1;
-        break;
-      }
-      else if (end < intervals[idx][1]) end = intervals[idx][1];
-    }
-  }
-  
-  if(!inserted) {
-    result[*returnSize] = (int*)malloc(sizeof(int) * 2);
-    result[*returnSize][0] = head;
-    result[*returnSize][1] = end;
-    (*returnColumnSizes)[*returnSize] = 2;
-    (*returnSize)++;    
-    return result;
-  }
-  
-  while(idx < intervalsSize){
-    result[*returnSize] = (int*)malloc(sizeof(int) * 2);
-    result[*returnSize][0] = intervals[idx][0];
-    result[*returnSize][1] = intervals[idx][1];
-    (*returnColumnSizes)[*returnSize] = 2;
-    (*returnSize)++;    
-    idx++;
-  }
-  return result;
 }
