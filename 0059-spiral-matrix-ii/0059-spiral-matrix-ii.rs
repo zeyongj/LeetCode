1d2b4537b00ffc7f87f494a804fb4aa7b937d54d
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-06-25 12:56:29 -0700

    Time: 1 ms (76.96%), Space: 2.1 MB (29.84%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.rs
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.rs
@@ -1,35 +1,47 @@
-func generateMatrix(n int) [][]int {
-    m := make([][]int, n)
-    for i := 0; i < n; i++ {
-        m[i] = make([]int, n)
+impl Solution {
+    pub fn generate_matrix(n: i32) -> Vec<Vec<i32>> {
+        let n = n as usize;
+        let mut res = vec![vec![0; n]; n];
+        let mut count = 0;
+        let (mut top, mut bottom, mut left, mut right) = (0, n - 1, 0, n - 1);
+        
+        while top <= bottom && left <= right {
+            for i in left..=right {
+                count += 1;
+                res[top][i] = count;
+            }
+            top += 1;
+            
+            for i in top..=bottom {
+                count += 1;
+                res[i][right] = count;
+            }
+            right -= 1;
+            if right == std::usize::MAX {
+                break;
+            }
+            
+            for i in (left..=right).rev() {
+                if top > bottom {
+                    continue;
+                }
+                count += 1;
+                res[bottom][i] = count;
+            }
+            bottom -= 1;
+            if bottom == std::usize::MAX {
+                break;
+            }
+            
+            for i in (top..=bottom).rev() {
+                if left > right {
+                    continue;
+                }
+                count += 1;
+                res[i][left] = count;
+            }
+            left += 1;
+        }
+        res
     }
-    RecSpiral(1, 0, n-1, m)
-    return m
-}
-
-func RecSpiral(current, start, end int, m [][]int) {
-    if start == end {
-        m[start][start] = current
-        return
-    }
-    if start > end {
-        return
-    }
-    for i := start; i <= end; i++ {
-        m[start][i] = current
-        current++
-    }
-    for i := start+1; i <= end; i++ {
-        m[i][end] = current
-        current++
-    }
-    for i := end-1; i >= start; i-- {
-        m[end][i] = current
-        current++
-    }
-    for i := end-1; i >= start+1; i-- {
-        m[i][start] = current
-        current++
-    }
-    RecSpiral(current, start+1, end-1, m)
 }
