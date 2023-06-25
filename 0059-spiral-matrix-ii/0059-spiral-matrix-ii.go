41d7a802aea7b668be7aee7e090e1cbce4faf21a
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-06-25 12:55:52 -0700

    Time: 2 ms (56.87%), Space: 2.1 MB (47.88%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.go
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.go
@@ -1,17 +1,35 @@
-# @param {Integer} n
-# @return {Integer[][]}
-def generate_matrix(n)
-  arr = Array.new(n){ Array.new(n) }
-  
-  i, j, di, dj = 0, 0, 0, 1
-  (0...n*n).each do |k|
-    arr[i][j] = k + 1  
-    
-    di, dj = dj, -di if arr[(i+di)%n][(j+dj)%n]
-    
-    i += di
-    j += dj
-  end
-  
-  arr
-end
+func generateMatrix(n int) [][]int {
+    m := make([][]int, n)
+    for i := 0; i < n; i++ {
+        m[i] = make([]int, n)
+    }
+    RecSpiral(1, 0, n-1, m)
+    return m
+}
+
+func RecSpiral(current, start, end int, m [][]int) {
+    if start == end {
+        m[start][start] = current
+        return
+    }
+    if start > end {
+        return
+    }
+    for i := start; i <= end; i++ {
+        m[start][i] = current
+        current++
+    }
+    for i := start+1; i <= end; i++ {
+        m[i][end] = current
+        current++
+    }
+    for i := end-1; i >= start; i-- {
+        m[end][i] = current
+        current++
+    }
+    for i := end-1; i >= start+1; i-- {
+        m[i][start] = current
+        current++
+    }
+    RecSpiral(current, start+1, end-1, m)
+}
