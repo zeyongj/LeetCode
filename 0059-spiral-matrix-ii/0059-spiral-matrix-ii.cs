ae017bd14ec3326e80d0d7cb647afcb7ba980930
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-06-25 12:54:39 -0700

    Time: 103 ms (29.13%), Space: 36.8 MB (28.54%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cs
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cs
@@ -3,19 +3,19 @@ public class Solution
     public int[][] GenerateMatrix(int n)
     {
         var matrix = new int[n][];
-        for (var i = 0; i < n; i++)
+        for (int i = 0; i < n; i++)
             matrix[i] = new int[n];
 
         var steps = new[] { 1, n, -1, -n };
-        var step = 0;
-        var curr = n;
-        var twice = 0;
-        var pos = 0;
-        var num = 1;
+        int step = 0;
+        int curr = n;
+        int twice = 0;
+        int pos = 0;
+        int num = 1;
 
         while (curr > 0)
         {
-            for (var i = 0; i < curr; i++)
+            for (int i = 0; i < curr; i++)
             {
                 matrix[pos / n][pos % n] = num++;
                 pos += steps[(i == curr - 1 ? ++step : step) % 4];
