9bd7c857201ccd1f81fe34cfd6f065a4bbc6b7aa
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-06-25 12:55:27 -0700

    Time: 97 ms (19.48%), Space: 210.9 MB (72.73%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.rb
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.rb
@@ -1,29 +1,17 @@
-public class Solution
-{
-    public int[][] GenerateMatrix(int n)
-    {
-        var matrix = new int[n][];
-        for (int i = 0; i < n; i++)
-            matrix[i] = new int[n];
-
-        var steps = new[] { 1, n, -1, -n };
-        int step = 0;
-        int curr = n;
-        int twice = 0;
-        int pos = 0;
-        int num = 1;
-
-        while (curr > 0)
-        {
-            for (int i = 0; i < curr; i++)
-            {
-                matrix[pos / n][pos % n] = num++;
-                pos += steps[(i == curr - 1 ? ++step : step) % 4];
-            }
-
-            curr -= ++twice % 2;
-        }
-
-        return matrix;
-    }
-}
+# @param {Integer} n
+# @return {Integer[][]}
+def generate_matrix(n)
+  arr = Array.new(n){ Array.new(n) }
+  
+  i, j, di, dj = 0, 0, 0, 1
+  (0...n*n).each do |k|
+    arr[i][j] = k + 1  
+    
+    di, dj = dj, -di if arr[(i+di)%n][(j+dj)%n]
+    
+    i += di
+    j += dj
+  end
+  
+  arr
+end
