03eca50fb6ee4c0b65bd072deb28cbf1c6e96d2e
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-06-26 13:52:56 -0700

    Time: 295 ms (51.02%), Space: 78.4 MB (30.61%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.js
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.js
@@ -1,56 +1,40 @@
-public class Solution {
-    public long TotalCost(int[] c, int k, int can)
-    {
-        long ans = 0;
-        if (c.Length == 1)
-            return c[0];
-
-        PriorityQueue<int,int> l = new();
-        PriorityQueue<int,int> r = new();
-        int left = 0, right = c.Length - 1;
-
-        for (int i = 1; i <= can; i++)
-        {
-            if (i - 1 >= c.Length - i)
-                break;
-
-            l.Enqueue(c[i - 1], c[i - 1]);
-            left = i;
-            r.Enqueue(c[c.Length - i], c[c.Length - i]);
-            right = c.Length - i - 1;
-        }
-
-        bool f = true;
-        while (k > 0)
-        {
-            if (left > right)
-                f = false;
-
-            int a = l.Count>0 ? l.Peek() : int.MaxValue;
-            int b = r.Count>0 ? r.Peek() : int.MaxValue;
-
-            if (a <= b)
-            {
-                ans += a;
-                l.Dequeue();
-                if (f)
-                    l.Enqueue(c[left],c[left]);
-                if (f)
-                    left++;
-            }
-            else
-            {
-                ans += b;
-                r.Dequeue();
-                if (f)
-                    r.Enqueue(c[right],c[right]);
-                if (f)
-                    right--;
-            }
-
-            k--;
-        }
-
-        return ans;
+/**
+ * @param {number[]} costs
+ * @param {number} k
+ * @param {number} candidates
+ * @return {number}
+ */
+var totalCost = function (costs, k, candidates) {
+  const pq = new MaxPriorityQueue({
+    compare: (a, b) => (a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]),
+  });
+  let start = 0;
+  let end = costs.length - 1;
+  let ans = 0;
+  while (start < Math.min(candidates, costs.length)) {
+    pq.enqueue([costs[start], start, true]);
+    start++;
+  }
+  start--;
+  while (end > start && end >= costs.length - candidates) {
+    pq.enqueue([costs[end], end, false]);
+    end--;
+  }
+  end++;
+  for (let i = 0; i < Math.min(k, costs.length); i++) {
+    const [cost, idx, fromStart] = pq.dequeue();
+    ans += cost;
+    if (fromStart) {
+      if (start < end - 1) {
+        start++;
+        pq.enqueue([costs[start], start, true]);
+      }
+    } else {
+      if (start < end - 1) {
+        end--;
+        pq.enqueue([costs[end], end, false]);
+      }
     }
-}
+  }
+  return ans;
+};
