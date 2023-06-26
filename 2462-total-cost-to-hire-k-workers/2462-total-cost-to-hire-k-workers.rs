9134d17723b643d33a3b134008d82085b8a1e918
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-06-26 13:54:51 -0700

    Time: 35 ms (44.83%), Space: 3.6 MB (51.72%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.rs
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.rs
@@ -1,67 +1,29 @@
-import "container/heap"
+use std::collections::BinaryHeap;
+use std::cmp::Reverse;
 
-type IntHeap []int
+impl Solution {
+    pub fn total_cost(costs: Vec<i32>, k: i32, candidates: i32) -> i64 {
+        let mut iter = costs.iter();
+        let mut heap = BinaryHeap::new();
+        for _ in 0..candidates {
+            if let Some(&x) = iter.next() {
+                heap.push((Reverse(x), true));
+            }
+            if let Some(&x) = iter.next_back() {
+                heap.push((Reverse(x), false));
+            }
+        }
 
-func (h IntHeap) Len() int           { return len(h) }
-func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
-func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-
-func (h *IntHeap) Push(x any) {
-
-	*h = append(*h, x.(int))
-}
-
-func (h *IntHeap) Pop() any {
-	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[0 : n-1]
-	return x
-}
-
-func (h IntHeap) Top() int {
-	return h[0]
-}
-
-func totalCost(costs []int, k int, candidates int) int64 {
-	i, j := 0, len(costs)-1
-	ans := int64(0)
-	upPq := &IntHeap{}
-	downPq := &IntHeap{}
-	heap.Init(upPq)
-	heap.Init(downPq)
-	for i < j && candidates > 0 {
-		heap.Push(downPq, costs[i])
-		heap.Push(upPq, costs[j])
-		i++
-		j--
-		candidates--
-	}
-	if i == j && candidates > 0 {
-		heap.Push(downPq, costs[i])
-		i++
-	}
-	for k > 0 {
-		if upPq.Len() == 0 {
-			ans += int64(heap.Pop(downPq).(int))
-		} else if downPq.Len() == 0 {
-			ans += int64(heap.Pop(upPq).(int))
-		} else {
-			if upPq.Top() < downPq.Top() {
-				ans += int64(heap.Pop(upPq).(int))
-				if j >= i {
-					heap.Push(upPq, costs[j])
-					j--
-				}
-			} else {
-				ans += int64(heap.Pop(downPq).(int))
-				if i <= j {
-					heap.Push(downPq, costs[i])
-					i++
-				}
-			}
-		}
-		k--
-	}
-	return ans
+        let mut ans = 0i64;
+        for _ in 0..k {
+            if let Some((Reverse(x), is_front)) = heap.pop() {
+                ans += x as i64;
+                let opt = if is_front { iter.next() } else { iter.next_back() };
+                if let Some(&y) = opt {
+                    heap.push((Reverse(y), is_front));
+                }
+            }
+        }
+        ans
+    }
 }
