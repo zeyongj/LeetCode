693bb79f7358240b6d318352d0bc41767fe20da8
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-06-26 13:53:21 -0700

    Time: 393 ms (100.00%), Space: 217.1 MB (100.00%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.rb
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.rb
@@ -1,40 +1,38 @@
-/**
- * @param {number[]} costs
- * @param {number} k
- * @param {number} candidates
- * @return {number}
- */
-var totalCost = function (costs, k, candidates) {
-  const pq = new MaxPriorityQueue({
-    compare: (a, b) => (a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]),
-  });
-  let start = 0;
-  let end = costs.length - 1;
-  let ans = 0;
-  while (start < Math.min(candidates, costs.length)) {
-    pq.enqueue([costs[start], start, true]);
-    start++;
-  }
-  start--;
-  while (end > start && end >= costs.length - candidates) {
-    pq.enqueue([costs[end], end, false]);
-    end--;
-  }
-  end++;
-  for (let i = 0; i < Math.min(k, costs.length); i++) {
-    const [cost, idx, fromStart] = pq.dequeue();
-    ans += cost;
-    if (fromStart) {
-      if (start < end - 1) {
-        start++;
-        pq.enqueue([costs[start], start, true]);
-      }
-    } else {
-      if (start < end - 1) {
-        end--;
-        pq.enqueue([costs[end], end, false]);
-      }
-    }
-  }
-  return ans;
-};
+# @param {Integer[]} costs
+# @param {Integer} k
+# @param {Integer} candidates
+# @return {Integer}
+def total_cost(costs, k, candidates)
+  right_costs = costs.pop(candidates).sort
+  left_costs = costs.shift(candidates).sort
+
+  k.times.sum do
+    if right_costs.empty?
+      use_left_costs(left_costs, costs)
+    elsif left_costs.empty?
+      use_right_costs(right_costs, costs)
+    elsif left_costs.first <= right_costs.first
+      use_left_costs(left_costs, costs)
+    else
+      use_right_costs(right_costs, costs)
+    end  
+  end
+end             
+
+def use_left_costs(left_costs, costs)
+  cost = left_costs.shift
+  unless costs.empty?
+    index = left_costs.bsearch_index {|num| num > costs.first} || left_costs.size
+    left_costs.insert(index, costs.shift)
+  end
+  cost
+end
+
+def use_right_costs(right_costs, costs)
+  cost = right_costs.shift
+  unless costs.empty?
+    index = right_costs.bsearch_index {|num| num > costs.last} || right_costs.size
+    right_costs.insert(index, costs.pop)
+  end
+  cost
+end
