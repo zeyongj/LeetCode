54885bb215427ab72f0dda712d79a1c5b419d8e6
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-06-28 14:02:15 -0700

    Time: 32 ms (99.29%), Space: 16.4 MB (46.73%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,16 +1,9 @@
-class Solution {
-public:
-    int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, 1)); // initialize all cells to 1
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[1]*n for _ in range(m)]
 
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                // Each cell can be reached either from the cell above it or the cell on its left.
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-            }
-        }
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
 
-        // The bottom right cell contains the number of unique paths to reach it.
-        return dp[m - 1][n - 1];
-    }
-};
+        return dp[-1][-1]
