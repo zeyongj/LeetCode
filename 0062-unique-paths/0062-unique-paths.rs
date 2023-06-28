c6cb0f288bff4699abe9d4fb33f4a6b13506d40b
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-06-28 14:06:59 -0700

    Time: 0 ms (100.00%), Space: 2 MB (58.87%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.rs
+++ 0062-unique-paths/0062-unique-paths.rs
@@ -1,24 +1,22 @@
-func uniquePaths(m int, n int) int {
-    
-    result := make([][]int, m)
-    for i:=0; i<m; i++{
-        result[i] = make([]int,n)
-    }
-    
-    for i:=0; i<m;i++{
-        result[i][0] = 1
-    }
-    
-    for j:=1; j<n; j++{
-        result[0][j] = 1
-    }
-    
-    
-    for i:=1;i<m; i++{
-        for j:=1; j<n; j++{
-            result[i][j] = result[i-1][j] + result[i][j-1]
+impl Solution {
+    pub fn unique_paths(m: i32, n: i32) -> i32 {
+        if m == 0 || n == 0 {
+            return 0;
         }
+
+        let m: usize = m as usize;
+        let n: usize = n as usize;
+        
+        let mut path: Vec<Vec<i32>> = vec![vec![0; n]; m];        
+        for i in 0..m {
+            for j in 0..n {
+                if i == 0 || j == 0 {
+                    path[i][j] = 1;
+                } else {
+                    path[i][j] = path[i - 1][j] + path[i][j - 1];
+                }
+            }
+        }
+        path[m - 1][n - 1]
     }
-    return result[m-1][n-1]
-    
 }
