0ec64fb85e93d389c6288052f6dce2f28749a8d0
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-06-28 14:05:09 -0700

    Time: 61 ms (54.87%), Space: 42.2 MB (58.30%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.js
+++ 0062-unique-paths/0062-unique-paths.js
@@ -1,32 +1,15 @@
-public class Solution {
-    public int UniquePaths(int m, int n)
-        {
-            // Matrix to store the value of possible ways to reach each cell
-            int[][] dp = new int[m][];
-
-            // C# thing, as we cannot define int[][] dp = new int[m][n]; 
-            for (int i = 0; i < m; i++)
-            {
-                dp[i] = new int[n];
-            }
-
-            //Base Conditions 
-            // all first column of every row as 1
-            for(int i = 0; i < dp.Length; i++)
-                dp[i][0] = 1;
-            // all first row of every column as 1
-            for(int i = 0; i< dp[0].Length; i++)
-                  dp[0][i] = 1;
-
-            for (int i = 1; i < m; i++)
-            {
-                for (int j = 1; j < n; j++)
-                {
-                        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];   
-                }
-            }
-
-            return dp[m - 1][n - 1];
-
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n) {
+    const res = [];
+    for (let i = 0; i < n; i++) res.push([...new Array(m).fill(1)]); // initialize list
+    for (let i = 1; i < n; i++) {
+        for (let j = 1; j < m; j++) {
+            res[i][j] = res[i-1][j] + res[i][j-1];
         }
-}
+    }
+    return res[n-1][m-1];
+};
