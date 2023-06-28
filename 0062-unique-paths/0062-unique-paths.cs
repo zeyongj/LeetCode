5a9db9a1a77cd5a8a4d4670fe70bc4849a0cc233
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-06-28 14:04:28 -0700

    Time: 31 ms (20.92%), Space: 26.7 MB (79.49%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cs
+++ 0062-unique-paths/0062-unique-paths.cs
@@ -1,17 +1,32 @@
-int uniquePaths(int m, int n){
-    int dp[m+1][n+1];
-    for(int i=0; i<m+1; i++)
-        for(int j=0; j<n+1; j++)
+public class Solution {
+    public int UniquePaths(int m, int n)
         {
-            if(i==1 || j==1)
-                dp[i][j] = 1;
-            else
-                dp[i][j] = 0;
+            // Matrix to store the value of possible ways to reach each cell
+            int[][] dp = new int[m][];
+
+            // C# thing, as we cannot define int[][] dp = new int[m][n]; 
+            for (int i = 0; i < m; i++)
+            {
+                dp[i] = new int[n];
+            }
+
+            //Base Conditions 
+            // all first column of every row as 1
+            for(int i = 0; i < dp.Length; i++)
+                dp[i][0] = 1;
+            // all first row of every column as 1
+            for(int i = 0; i< dp[0].Length; i++)
+                  dp[0][i] = 1;
+
+            for (int i = 1; i < m; i++)
+            {
+                for (int j = 1; j < n; j++)
+                {
+                        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];   
+                }
+            }
+
+            return dp[m - 1][n - 1];
+
         }
-    for(int i=2; i<=m; i++)
-        for(int j=2; j<=n; j++)
-        {
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
-        }
-    return dp[m][n];
 }
