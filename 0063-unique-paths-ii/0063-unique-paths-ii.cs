b8ddd0969277c162b3933077bad9c7320d73fbc8
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-06-30 10:34:38 -0700

    Time: 99 ms (16.39%), Space: 38.5 MB (76.59%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cs
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cs
@@ -1,30 +1,31 @@
-int uniquePathsWithObstacles(int** obstacleGrid, int obstacleGridSize, int* obstacleGridColSize){
-    int* list = malloc(sizeof(int)*(*obstacleGridColSize));
-    for (int i = 0 ; i < *obstacleGridColSize ; i++){
-        if (obstacleGrid[0][i] == 1){
-            list[i] = 0;
-            i++;
-            while (i < *obstacleGridColSize){
-                list[i] = 0;
-                i++;
-            } 
-        } else {
-            list[i] = 1;
-        }
+public class Solution {
+    public int UniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid == null || obstacleGrid.Length == 0 || obstacleGrid[0][0] == 1)
+            return 0;            
+        
+        int[,] grid = new int[obstacleGrid.Length, obstacleGrid[0].Length];
+        
+        grid[0, 0] = 1;
+        
+        for (int i = 1; i < obstacleGrid[0].Length; i++)
+            if (obstacleGrid[0][i] != 1)
+                grid[0, i] = 1;
+            else
+                break;
+        
+        for (int i = 1; i < obstacleGrid.Length; i++)
+            if (obstacleGrid[i][0] != 1)
+                grid[i, 0] = 1;
+            else
+                break;
+        
+        for (int i = 1; i < obstacleGrid.Length; i++)
+            for (int j = 1; j < obstacleGrid[0].Length; j++)
+                if (obstacleGrid[i][j] == 1)
+                    continue;
+                else
+                    grid[i, j] = grid[i - 1, j] + grid[i, j - 1];
+        
+        return grid[obstacleGrid.Length - 1, obstacleGrid[0].Length - 1];
     }
-    for (int i = 1 ; i < obstacleGridSize ; i++){
-        if (obstacleGrid[i][0] == 1){
-            list[0] = 0;
-        }
-        for (int j = 1 ; j < *obstacleGridColSize ; j++){
-            if (obstacleGrid[i][j] == 1){
-                list[j] = 0;
-            } else {
-                list[j] += list[j-1];
-            }
-        }
-    }
-    int ans = list[*obstacleGridColSize-1];
-    free(list);
-    return ans < 2000000000 ? ans : 2000000000;
 }
