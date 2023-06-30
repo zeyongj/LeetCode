0ae50c3ebabf49c25b390ab3ef3ac546371fc513
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-06-30 10:39:00 -0700

    Time: 169 ms (32.88%), Space: 34.5 MB (80.82%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.kt
+++ 0063-unique-paths-ii/0063-unique-paths-ii.kt
@@ -1,27 +1,18 @@
-impl Solution {
-    pub fn unique_paths_with_obstacles(obstacle_grid: Vec<Vec<i32>>) -> i32 {
-        let (m, n) = (obstacle_grid.len(), obstacle_grid[0].len());
-        if obstacle_grid[0][0] == 1 || obstacle_grid[m - 1][n - 1] == 1 {
-            return 0;
-        }
-
-        let mut dp = vec![vec![0; n]; m];
-        dp[0][0] = 1;
-
-        obstacle_grid.iter().enumerate().for_each(|(i, row)| {
-            row.iter()
-                .enumerate()
-                .filter(|(_, x)| **x == 0)
-                .for_each(|(j, _)| {
-                    if i > 0 {
-                        dp[i][j] += dp[i - 1][j];
-                    }
-                    if j > 0 {
-                        dp[i][j] += dp[i][j - 1];
-                    }
-                })
-        });
-
-        dp[m - 1][n - 1]
+class Solution {
+    fun uniquePathsWithObstacles(obstacleGrid: Array<IntArray>): Int {
+        val m = obstacleGrid.size
+        val n = obstacleGrid[0].size
+        val matrix = Array(m) { IntArray(n) }
+        for (i in m - 1 downTo 0)
+            for (j in n - 1 downTo 0) {
+                matrix[i][j] = when {
+                    obstacleGrid[i][j] == 1 -> 0
+                    i == m - 1 && j == n - 1 -> 1
+                    i == m - 1 -> matrix[i][j + 1]
+                    j == n - 1 -> matrix[i + 1][j]
+                    else -> matrix[i + 1][j] + matrix[i][j + 1]
+                }
+            }
+        return matrix[0][0]
     }
 }
