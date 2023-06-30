cbea70922dda6a71f83d41615c9e1af2632e62ce
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-06-30 10:37:50 -0700

    Time: 1 ms (81.40%), Space: 2.1 MB (37.21%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.rs
+++ 0063-unique-paths-ii/0063-unique-paths-ii.rs
@@ -1,29 +1,27 @@
-func uniquePathsWithObstacles(obstacleGrid [][]int) int {
-    if obstacleGrid[0][0] == 1 {
-        return 0
-    }
-    x := 1
-    for i := range obstacleGrid {
-        if obstacleGrid[i][0] == 1 {
-            x = 0
+impl Solution {
+    pub fn unique_paths_with_obstacles(obstacle_grid: Vec<Vec<i32>>) -> i32 {
+        let (m, n) = (obstacle_grid.len(), obstacle_grid[0].len());
+        if obstacle_grid[0][0] == 1 || obstacle_grid[m - 1][n - 1] == 1 {
+            return 0;
         }
-        obstacleGrid[i][0] = x
+
+        let mut dp = vec![vec![0; n]; m];
+        dp[0][0] = 1;
+
+        obstacle_grid.iter().enumerate().for_each(|(i, row)| {
+            row.iter()
+                .enumerate()
+                .filter(|(_, x)| **x == 0)
+                .for_each(|(j, _)| {
+                    if i > 0 {
+                        dp[i][j] += dp[i - 1][j];
+                    }
+                    if j > 0 {
+                        dp[i][j] += dp[i][j - 1];
+                    }
+                })
+        });
+
+        dp[m - 1][n - 1]
     }
-    x = 1
-    for i := 1; i < len(obstacleGrid[0]); i++ {
-        if obstacleGrid[0][i] == 1 {
-            x = 0
-        }
-        obstacleGrid[0][i] = x
-    }
-    for i := 1; i < len(obstacleGrid); i++ {
-        for j := 1; j < len(obstacleGrid[0]); j++ {
-            if obstacleGrid[i][j] == 1 {
-                obstacleGrid[i][j] = 0
-                continue
-            }
-            obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1]
-        }
-    }
-    return obstacleGrid[len(obstacleGrid)-1][len(obstacleGrid[0])-1]
 }
