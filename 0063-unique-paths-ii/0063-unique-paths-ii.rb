4379bd5c9ba0738d778e04eb9e3540ae5975d943
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-06-30 10:36:24 -0700

    Time: 102 ms (10.71%), Space: 211.2 MB (7.14%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.rb
+++ 0063-unique-paths-ii/0063-unique-paths-ii.rb
@@ -1,19 +1,20 @@
-/**
- * @param {number[][]} obstacleGrid
- * @return {number}
- */
-var uniquePathsWithObstacles = function(obstacleGrid) {
-    let m = obstacleGrid[0].length
-    let board = new Array(m).fill(0)
-    board[0] = 1
-    for (let row of obstacleGrid) {
-        for (let c = 0; c < m; c++) {
-            if (row[c]) {
-                board[c] = 0
-            } else if (c > 0) {
-                board[c] += board[c - 1]
-            }  
-        }
-    }
-    return board[m - 1]
-};
+# @param {Integer[][]} obstacle_grid
+# @return {Integer}
+def unique_paths_with_obstacles(obstacle_grid)
+  m = obstacle_grid.size
+  n = obstacle_grid.first.size
+  row = [0] * n
+  for i in (0...n)
+    if obstacle_grid.first[i] == 1
+      break
+    else
+      row[i] = 1
+    end
+  end
+  for i in (1...m)
+    for j in (0...n)
+      row[j] = obstacle_grid[i][j] == 0 ? (j > 0 ? row[j - 1] : 0) + row[j] : 0
+    end
+  end
+  row.last
+end
