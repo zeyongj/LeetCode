42b027599298b56d02f3e745a968be1c82ff9dc3
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-07-02 14:49:07 -0700

    Time: 14 ms (60.78%), Space: 7.4 MB (51.63%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.c
+++ 0064-minimum-path-sum/0064-minimum-path-sum.c
@@ -1,26 +1,25 @@
-class Solution:
-    def minPathSum(self, grid):
-        m = len(grid)
-        n = len(grid[0])
+/*int min(int a,int b)
+{
+    return a>b?b:a;
+}*/
 
-        # Initialize the dynamic programming table with the size of grid
-        dp = [[0]*n for _ in range(m)]
-
-        # Initialize the top left cell
-        dp[0][0] = grid[0][0]
-
-        # Calculate the minimum path sum for the first row
-        for j in range(1, n):
-            dp[0][j] = dp[0][j-1] + grid[0][j]
-
-        # Calculate the minimum path sum for the first column
-        for i in range(1, m):
-            dp[i][0] = dp[i-1][0] + grid[i][0]
-
-        # Calculate the minimum path sum for each remaining cell
-        for i in range(1, m):
-            for j in range(1, n):
-                dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
-
-        # Return the minimum path sum to reach the bottom right cell
-        return dp[-1][-1]
+int minPathSum(int** grid, int gridSize, int* gridColSize){
+    int i,j;
+    for(i=1;i<*gridColSize;i++)
+    {
+        grid[0][i]+=grid[0][i-1];
+    }
+    for(i=1;i<gridSize;i++)
+    {
+        grid[i][0]+=grid[i-1][0];
+    }
+    for(i=1;i<gridSize;i++)
+    {
+        for(j=1;j<*gridColSize;j++)
+        {
+            //grid[i][j]+=min(grid[i-1][j],grid[i][j-1]);
+            grid[i][j]+=grid[i-1][j]>grid[i][j-1]?grid[i][j-1]:grid[i-1][j];
+        }
+    }
+    return grid[gridSize-1][*gridColSize-1];
+}
