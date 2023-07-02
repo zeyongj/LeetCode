f8a784d72bcaa0a49ac2bcc778a374cf2ed32073
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-07-02 14:51:58 -0700

    Time: 91 ms (72.41%), Space: 212.2 MB (72.41%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.rb
+++ 0064-minimum-path-sum/0064-minimum-path-sum.rb
@@ -1,20 +1,18 @@
-/**
- * @param {number[][]} grid
- * @return {number}
- */
-var minPathSum = function(grid) {
-    for(let i = 1; i < grid[0].length; i++) {
-        grid[0][i] += grid[0][i - 1]
-    }
-    for(let i = 1; i < grid.length; i++) {
-        grid[i][0] += grid[i - 1][0]
-    }
-    
-    for(let i = 1; i < grid.length; i++) {
-        for(let j = 1; j < grid[0].length; j++){
-            grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]) 
-        }
-    }
-    
-    return grid[grid.length - 1][grid[0].length - 1]
-};
+# @param {Integer[][]} grid
+# @return {Integer}
+def min_path_sum(grid)
+  grid.each_index do |i|
+    grid.first.each_index do |j|
+      if i == 0 && j == 0
+        grid[i][j] = grid[i][j]
+      elsif i == 0 && j > 0
+        grid[i][j] = grid[i][j] + grid[i][j-1]
+      elsif j == 0 && i > 0
+        grid[i][j] = grid[i][j] + grid[i-1][j]
+      else
+        grid[i][j] = grid[i][j] + [grid[i][j-1], grid[i-1][j]].min
+      end
+    end
+  end
+  grid[-1][-1]
+end
