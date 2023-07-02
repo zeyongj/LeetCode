e214f088746afe47c318d8264164f683e541e2cb
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-07-02 14:54:14 -0700

    Time: 164 ms (100.00%), Space: 37.2 MB (86.67%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.kt
+++ 0064-minimum-path-sum/0064-minimum-path-sum.kt
@@ -1,20 +1,15 @@
-object Solution {
-  def minPathSum(grid: Array[Array[Int]]): Int = {
-    (grid.length, grid.headOption.map(_.length).getOrElse(0)) match {
-      case (m, n) if m > 0 && n > 0 =>
-        val dp = Array.fill(m, n)(grid(m - 1)(n - 1))
-        for {
-          k <- 1 to m + n
-          i <- math.max(0, (m - 1) - k) to math.min(m - 1, m + n - k - 2)
-        } {
-          val j = m + n - i - k - 2
-          dp(i)(j) = grid(i)(j) + Seq((i + 1, j), (i, j + 1))
-            .collect {
-              case (x, y) if dp.isDefinedAt(x) && dp(x).isDefinedAt(y) => dp(x)(y)
-            }
-            .min
-        }
-        dp(0)(0)
-    }
+class Solution {
+  fun minPathSum(grid: Array<IntArray>): Int {
+    val dp = IntArray(grid[0].size)
+    dp[0] = grid[0][0]
+    for (i in 1 until dp.size)
+      dp[i] = dp[i - 1] + grid[0][i]
+    
+    for (r in 1 until grid.size) {
+      dp[0] += grid[r][0]
+      for (j in 1 until dp.size)
+        dp[j] = minOf(dp[j], dp[j - 1]) + grid[r][j]
+    }    
+    return dp[dp.size - 1]
   }
 }
