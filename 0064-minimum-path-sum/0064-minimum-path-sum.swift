be3856f23e9844aeb676aafea67af0d6bfcbc839
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-07-02 14:52:24 -0700

    Time: 51 ms (82.47%), Space: 14.4 MB (77.32%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.swift
+++ 0064-minimum-path-sum/0064-minimum-path-sum.swift
@@ -1,18 +1,25 @@
-# @param {Integer[][]} grid
-# @return {Integer}
-def min_path_sum(grid)
-  grid.each_index do |i|
-    grid.first.each_index do |j|
-      if i == 0 && j == 0
-        grid[i][j] = grid[i][j]
-      elsif i == 0 && j > 0
-        grid[i][j] = grid[i][j] + grid[i][j-1]
-      elsif j == 0 && i > 0
-        grid[i][j] = grid[i][j] + grid[i-1][j]
-      else
-        grid[i][j] = grid[i][j] + [grid[i][j-1], grid[i-1][j]].min
-      end
-    end
-  end
-  grid[-1][-1]
-end
+class Solution {
+    func minPathSum(_ grid: [[Int]]) -> Int {
+        let m = grid.count
+        let n = grid[0].count
+        var cost = Array(repeating:Array(repeating:0,count:n),count:m)
+        cost[0][0] = grid[0][0]
+		
+        //populating the first col 
+        for i in 1..<m {
+            cost[i][0] = cost[i-1][0] + grid[i][0]
+        }
+        
+        //populating first row
+        for i in 1..<n {
+            cost[0][i] = cost[0][i-1] + grid[0][i]
+        }
+        
+        for i in 1..<m {
+            for j in 1..<n {
+                cost[i][j] = grid[i][j] + min(cost[i-1][j], cost[i][j-1])
+            }
+         }
+        return cost[m-1][n-1]
+    }
+}
