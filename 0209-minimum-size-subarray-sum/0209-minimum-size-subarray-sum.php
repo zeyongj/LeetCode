3f49ea0a9a80f39ae207cd044574e14fc7ddd2c1
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-07-06 12:34:22 -0700

    Time: 91 ms (12.50%), Space: 31.4 MB (91.67%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.php
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.php
@@ -1,17 +1,25 @@
-impl Solution {
-    pub fn min_sub_array_len(target: i32, nums: Vec<i32>) -> i32 {
-        let hi = nums.iter().enumerate();
-        let mut lo = hi.clone();
-        let mut sum = 0;
-        let mut min_len = None;
-        for (hi_index, hi_num) in hi {
-            sum += hi_num;
-            while sum >= target {
-                let (lo_index, lo_num) = lo.next().unwrap();
-                min_len = Some(min_len.unwrap_or(usize::MAX).min(hi_index - lo_index + 1));
-                sum -= lo_num;
+class Solution
+{
+
+    /**
+     * @param Integer   $target
+     * @param Integer[] $nums
+     *
+     * @return Integer
+     */
+    function minSubArrayLen($target, $nums)
+    {
+        $len = PHP_INT_MAX;
+
+        $count = count($nums);
+        for ($i = $j = $s = 0; $i < $count;) {
+            $s += $nums[$i++];
+            while ($s >= $target) {
+                $len = min($len, $i - $j);
+                $s -= $nums[$j++];
             }
         }
-        min_len.unwrap_or(0) as i32
+
+        return $len < PHP_INT_MAX ? $len : 0;
     }
 }
