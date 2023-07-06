5082a7d9519932c07bbc40b57ab083057bd81dae
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-07-06 12:33:49 -0700

    Time: 3 ms (93.10%), Space: 3.1 MB (22.99%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.rs
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.rs
@@ -1,13 +1,17 @@
-class Solution {
-    fun minSubArrayLen(target: Int, nums: IntArray): Int {
-        var lo = 0
-        var sum = 0
-        return nums.asSequence().mapIndexed { hi, n ->
-          sum += n
-          while (sum - nums[lo] >= target) sum -= nums[lo++]
-          (hi - lo + 1).takeIf { sum >= target }
+impl Solution {
+    pub fn min_sub_array_len(target: i32, nums: Vec<i32>) -> i32 {
+        let hi = nums.iter().enumerate();
+        let mut lo = hi.clone();
+        let mut sum = 0;
+        let mut min_len = None;
+        for (hi_index, hi_num) in hi {
+            sum += hi_num;
+            while sum >= target {
+                let (lo_index, lo_num) = lo.next().unwrap();
+                min_len = Some(min_len.unwrap_or(usize::MAX).min(hi_index - lo_index + 1));
+                sum -= lo_num;
+            }
         }
-        .filterNotNull()
-        .min() ?: 0
+        min_len.unwrap_or(0) as i32
     }
 }
