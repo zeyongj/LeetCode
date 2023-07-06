01fa91e7185b1f3524fd7e5d81531b987ab88576
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-07-06 12:28:57 -0700

    Time: 69 ms (51.19%), Space: 46.6 MB (16.25%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
@@ -1,19 +1,17 @@
-public class Solution {
-    public int MinSubArrayLen(int target, int[] nums) {
-        int left = 0, right = 0;
-        int sum = 0;
-        int min_len = Int32.MaxValue;
-        
-        while(right < nums.Length){
-            sum += nums[right];
-            
-            while(sum >= target){
-                min_len = Math.Min(min_len, right - left + 1);
-                sum -= nums[left];
-                left++;
-            }
-            right++;
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSubArrayLen = function(s, nums) {
+    let result = Number.MAX_SAFE_INTEGER
+    let sum = 0, left = 0
+    for(let i = 0; i < nums.length; i++){
+        sum += nums[i]
+        while(sum >= s){
+            result = Math.min(result, i - left + 1)
+            sum -= nums[left++]
         }
-        return (min_len != Int32.MaxValue) ? min_len : 0;                
     }
-}
+    return result === Number.MAX_SAFE_INTEGER ? 0 : result 
+};
