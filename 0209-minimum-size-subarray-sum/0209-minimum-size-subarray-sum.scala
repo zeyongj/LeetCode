fd16b841667f51ce81e38fe0346a01591c6802bd
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-07-06 12:32:43 -0700

    Time: 733 ms (36.84%), Space: 83.3 MB (15.00%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.scala
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.scala
@@ -1,20 +1,15 @@
-func minSubArrayLen(target int, nums []int) int {
-    	left, currSum := 0, 0
-
-	minLen := float64(len(nums) + 1)
-
-	for right := 0; right < len(nums); right++ {
-		currSum += nums[right]
-		for currSum >= target {
-			minLen = math.Min(minLen, float64(right-left+1))
-			currSum -= nums[left]
-			left++
-		}
-
-	}
-	if minLen == float64(len(nums)+1) {
-		return 0
-	}
-
-	return int(minLen)
+object Solution {
+    
+    def process(sums: Array[Int], left: Int, index: Int, min: Int, target: Int): Int = {
+        if (index >= sums.length) min
+        else if (sums(index) - sums(left) < target) {
+             process(sums, left, index + 1, min, target)
+        } else process(sums, left + 1, index, min.min(index - left), target)
+    }
+    
+    def minSubArrayLen(target: Int, nums: Array[Int]): Int = {
+        val sums = nums.scanLeft(0)(_ + _)
+        val result = process(sums, left = 0, index = 0, Int.MaxValue, target)
+        if (result == Int.MaxValue) 0 else result
+    }
 }
