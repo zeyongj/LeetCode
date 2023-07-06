57056095dc29cf2e4cd659d931f71e680b4b64ca
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-07-06 12:29:23 -0700

    Time: 102 ms (81.82%), Space: 213.7 MB (100.00%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.rb
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.rb
@@ -1,17 +1,25 @@
-/**
- * @param {number} target
- * @param {number[]} nums
- * @return {number}
- */
-var minSubArrayLen = function(s, nums) {
-    let result = Number.MAX_SAFE_INTEGER
-    let sum = 0, left = 0
-    for(let i = 0; i < nums.length; i++){
-        sum += nums[i]
-        while(sum >= s){
-            result = Math.min(result, i - left + 1)
-            sum -= nums[left++]
-        }
-    }
-    return result === Number.MAX_SAFE_INTEGER ? 0 : result 
-};
+# @param {Integer} target
+# @param {Integer[]} nums
+# @return {Integer}
+def min_sub_array_len(s, nums)
+    return 0 if nums.empty?
+    min = nil
+    i,j = 0, 0
+    sum = nums[i]
+    while j < nums.size
+        if(sum < s)
+            j += 1
+            sum += nums[j] if j < nums.size
+            next
+        end
+        if(i==j)
+            return 1
+        end
+        length = j-i + 1
+        min = length if min.nil?
+        min = length if min > length
+        i += 1
+        sum -= nums[i-1]
+    end
+    min ? min : 0
+end
