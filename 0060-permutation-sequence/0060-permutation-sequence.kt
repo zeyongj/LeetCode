af18f99c7d1d5dfa13cd45c68e76eab3eb96986c
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-07-08 16:26:48 -0700

    Time: 138 ms (96.43%), Space: 33.8 MB (92.86%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.kt
+++ 0060-permutation-sequence/0060-permutation-sequence.kt
@@ -1,54 +1,18 @@
 class Solution {
-    /**
-     * @param Integer $n
-     * @param Integer $k
-     * @return String
-     */
-    function getPermutation($n, $k) {
-        if ($n == 0) return '';
-        
-        $numbers = range(1, $n);
-        $isVisited = array_fill(0, $n, false);
-        $result = [];
-        $numberGenerated = 0;
-        
-        $this->generateSequence($result, [], $isVisited, $numbers, $numberGenerated, $k);
-        
-        return implode('', $result);
+    fun getPermutation(n: Int, k: Int): String {
+        val s = MutableList<String>(n){it->(it+1).toString()}
+        return permute(s, n, k-1)
     }
-
-    function generateSequence(&$array, $currentSequence, &$isVisited, $numbers, &$numberGenerated, $k) {
-        if ($k <= $numberGenerated) {
-            return;
+    fun permute(s: MutableList<String>, n: Int, k: Int): String{
+        if(n==1){
+            return s[0]
         }
-        
-        if (count($currentSequence) == count($numbers)) {
-            $array = $currentSequence;
-            $numberGenerated++;
-            return;
-        }
-        
-        $remaining = count($numbers) - count($currentSequence);
-        $factorial = $this->factorial($remaining - 1);
-        
-        for ($i = 0; $i < count($numbers); $i++) {
-            if (!$isVisited[$i]) {
-                if ($k > $numberGenerated + $factorial) {
-                    $numberGenerated += $factorial;
-                    continue;
-                }
-                
-                $isVisited[$i] = true;
-                $currentSequence[] = $numbers[$i];
-                $this->generateSequence($array, $currentSequence, $isVisited, $numbers, $numberGenerated, $k);
-                $isVisited[$i] = false;
-                array_pop($currentSequence);
-            }
+        else{
+            var l = getFactorial(n-1)
+            return s.removeAt(k/l) + permute(s, n-1, k%l)
         }
     }
-    
-    function factorial($n) {
-        if ($n <= 1) return 1;
-        return $n * $this->factorial($n - 1);
+    fun getFactorial(n: Int): Int{
+        return if(n==1) 1 else n*getFactorial(n-1)
     }
 }
