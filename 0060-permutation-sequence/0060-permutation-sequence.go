45dc78132d64c015348c7bbc4146a872e986db0b
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-07-08 16:23:25 -0700

    Time: 3 ms (46.03%), Space: 2 MB (31.75%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.go
+++ 0060-permutation-sequence/0060-permutation-sequence.go
@@ -1,29 +1,26 @@
-class Solution {
-    func getPermutation(_ n: Int, _ k: Int) -> String {
-        var digitals: [Int] = []
-        var res: String = ""
-        var val = k
-        var m = n
-
-        for i in 1...n { digitals.append(i) }
-
-        while res.count < n && val > 0 {
-            let i = Int(ceil(Double(val) / Double(permutation(m - 1))))
-            res += "\(digitals[i-1])"
-            print(i, digitals)
-            digitals.remove(at: i - 1)
-            val -= (i - 1) * permutation(m - 1)
-            m -= 1
-        }
-        return res
-    }
-
-
-    func permutation(_ n: Int) -> Int {
-        guard n > 1 else { return 1 }
-        var res = 1
-        for i in 1...n { res *= i }
-        return res
+func getPermutation(n int, k int) string {
+    
+    list := make([]string,n)
+    for i := 1; i <= n; i++ {
+        list[i-1] = strconv.Itoa(i)
     }
     
+    factorial := make([]int,n)
+    factorial[0] = 1
+    for i := 1; i < n; i++ {
+        factorial[i] = i * factorial[i - 1]
+    }
+    
+    result := ""
+    k = k - 1
+    
+    for n > 0 {
+        pos := k / factorial[n - 1]
+        result = result + list[pos]
+        list = append(list[:pos], list[pos + 1:]...)
+        k = k % factorial[n - 1]
+        n = n - 1
+    }
+    
+    return result
 }
