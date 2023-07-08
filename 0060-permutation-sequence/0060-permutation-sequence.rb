801f2b7edd26b6b7dea27c2f4fb711f43fdf754d
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-07-08 16:20:37 -0700

    Time: 96 ms (50.00%), Space: 211.2 MB (50.00%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.rb
+++ 0060-permutation-sequence/0060-permutation-sequence.rb
@@ -1,26 +1,17 @@
-public class Solution {
-    public string GetPermutation(int n, int k) {
-      
-        int[] factorial = new int[n+1];
-        List<int> arr = new List<int>();
-        
-        factorial[0] = 1;
-        for(int i = 1; i <= n; i++)
-        {
-            factorial[i] = factorial[i-1] * i;
-            arr.Add(i);
-        }
-        
-        k--;
-        StringBuilder sb = new StringBuilder();
-        for(int i = 1; i <= n; i++)
-        {
-            int index = k / factorial[n - i];
-            sb.Append(arr[index]);
-            arr.RemoveAt(index);
-            k -= index * factorial[n - i];
-        }
-        
-        return sb.ToString();       
-    }
-}
+# @param {Integer} n
+# @param {Integer} k
+# @return {String}
+def get_permutation(n, k)
+    numbers = (1..n).to_a
+    k -= 1
+    permutation = ''
+    
+    while n > 0 do
+        n -= 1
+        factorial = Math.gamma(n+1).to_i  # gamma(n) = (n-1)!
+        index, k = k.divmod(factorial)
+        permutation += numbers.delete_at(index).to_s
+    end
+    
+    return permutation
+end
