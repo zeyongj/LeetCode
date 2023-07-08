aafb9de3704dc78f8fb8690f6e2301341820c03a
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-07-08 16:22:49 -0700

    Time: 7 ms (33.33%), Space: 14.1 MB (16.67%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.swift
+++ 0060-permutation-sequence/0060-permutation-sequence.swift
@@ -1,17 +1,29 @@
-# @param {Integer} n
-# @param {Integer} k
-# @return {String}
-def get_permutation(n, k)
-    numbers = (1..n).to_a
-    k -= 1
-    permutation = ''
+class Solution {
+    func getPermutation(_ n: Int, _ k: Int) -> String {
+        var digitals: [Int] = []
+        var res: String = ""
+        var val = k
+        var m = n
+
+        for i in 1...n { digitals.append(i) }
+
+        while res.count < n && val > 0 {
+            let i = Int(ceil(Double(val) / Double(permutation(m - 1))))
+            res += "\(digitals[i-1])"
+            print(i, digitals)
+            digitals.remove(at: i - 1)
+            val -= (i - 1) * permutation(m - 1)
+            m -= 1
+        }
+        return res
+    }
+
+
+    func permutation(_ n: Int) -> Int {
+        guard n > 1 else { return 1 }
+        var res = 1
+        for i in 1...n { res *= i }
+        return res
+    }
     
-    while n > 0 do
-        n -= 1
-        factorial = Math.gamma(n+1).to_i  # gamma(n) = (n-1)!
-        index, k = k.divmod(factorial)
-        permutation += numbers.delete_at(index).to_s
-    end
-    
-    return permutation
-end
+}
