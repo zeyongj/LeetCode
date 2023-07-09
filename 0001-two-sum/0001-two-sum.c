acd191871281a6b77d75258b66512dfb8e73b92c
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-07-09 12:36:51 -0700

    Time: 10 ms (95.78%), Space: 6.9 MB (7.68%) - LeetHub

--- 0001-two-sum/0001-two-sum.c
+++ 0001-two-sum/0001-two-sum.c
@@ -1,12 +1,40 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        numMap = {}
-        n = len(nums)
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
 
-        for i in range(n):
-            complement = target - nums[i]
-            if complement in numMap:
-                return [numMap[complement], i]
-            numMap[nums[i]] = i
+struct intPair {
+    int x;
+    int order;
+};
 
-        return []  # No solution found
+int compare(const void *a, const void *b) {
+    struct intPair *p = (struct intPair *) a;
+    struct intPair *q = (struct intPair *) b;
+    return p->x - q->x;
+}
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+    int i, j, *output;
+    output = malloc(2 * sizeof(int));
+    *returnSize = 2;
+    struct intPair *arr = malloc(numsSize * sizeof(struct intPair));
+    for (i = 0; i < numsSize; ++i) {
+        arr[i].x = nums[i];
+        arr[i].order = i;
+    }
+    qsort(arr, numsSize, sizeof(struct intPair), compare);
+    i = 0;
+    j = numsSize - 1;
+    while (arr[i].x + arr[j].x != target) {
+        while (arr[i].x + arr[j].x < target) {
+            ++i;
+        }
+        while (arr[i].x + arr[j].x > target) {
+            --j;
+        }
+    }
+    output[0] = arr[i].order;
+    output[1] = arr[j].order;
+    free(arr);
+    return output;
+}
