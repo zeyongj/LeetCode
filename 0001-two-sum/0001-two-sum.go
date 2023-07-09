4a02b288cb2b76ec745945ea5776395e6082620a
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-07-09 12:39:18 -0700

    Time: 9 ms (63.94%), Space: 4.2 MB (61.36%) - LeetHub

--- 0001-two-sum/0001-two-sum.go
+++ 0001-two-sum/0001-two-sum.go
@@ -1,15 +1,11 @@
-class Solution {
-    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-        var dict = [Int: Int]()
-        
-        for (i, num) in nums.enumerated() {
-            if let index = dict[target-num] {
-                if i != index {
-                    return [index, i]
-                }
-            }
-            dict[num] = i
-        }
-        return[0]
+func twoSum(nums []int, target int) []int {
+    m := make(map[int]int, len(nums))
+    
+    for i, num := range nums {
+        if idx, ok := m[target - num]; ok {
+            return []int{idx, i}            
+        } 
+        m[num] = i
     }
+    return []int{0, 0}
 }
