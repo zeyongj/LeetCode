541436a5288f7f9d9ec63a58e9c8c6e4c1a43b0d
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-07-10 13:37:26 -0700

    Time: 193 ms (78.97%), Space: 75.7 MB (56.67%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.c
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.c
@@ -1,26 +1,20 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def minDepth(self, root):
-        if root is None:
-            return 0
-
-        queue = deque([(root, 1)])
-
-        while queue:
-            node, depth = queue.popleft()
-
-            if node.left is None and node.right is None:
-                return depth
-
-            if node.left is not None:
-                queue.append((node.left, depth + 1))
-
-            if node.right is not None:
-                queue.append((node.right, depth + 1))
-
-        return -1
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int minDepth(struct TreeNode* root){
+    if (!root) return 0;
+    
+    if (!root->left && !root->right) return 1;
+    
+    if(!root->left) return minDepth(root->right)+1;
+    
+    if(!root->right) return minDepth(root->left)+1;
+    
+    int l = minDepth(root->left), r = minDepth(root->right);
+    return l > r ? r+1 : l+1;
+}
