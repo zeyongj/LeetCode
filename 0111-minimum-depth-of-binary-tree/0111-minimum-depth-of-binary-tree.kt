813301737a0d8f0479087c529b15743136c76c62
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-07-10 13:41:35 -0700

    Time: 522 ms (40.16%), Space: 57.5 MB (68.03%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.kt
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.kt
@@ -1,20 +1,25 @@
 /**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
  * Definition for a binary tree node.
- * class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
- *   var value: Int = _value
- *   var left: TreeNode = _left
- *   var right: TreeNode = _right
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
  * }
  */
-object Solution {
-    def minDepth(root: TreeNode): Int = {
-        if(root == null) return 0
-        helper(List(root))
-    }
-    
-    def helper(list: List[TreeNode]): Int = {
-        if(list.isEmpty) return 0
-        if(list.exists(o => o.left == null && o.right == null)) 1
-        else helper(list.flatMap(o => List(o.left, o.right).filterNot(_ == null))) + 1
+class Solution {
+    fun minDepth(root: TreeNode?): Int = with(ArrayDeque<TreeNode>()) {
+      root?.let { add(it) }
+      generateSequence(1) { (it + 1).takeIf { isNotEmpty() } }
+        .firstOrNull {
+          (1..size).any {
+            with(poll()) {
+              left?.let { add(it) }
+              right?.let { add(it) }
+              left == null && right == null
+            }
+          }
+        } ?: 0
     }
 }
