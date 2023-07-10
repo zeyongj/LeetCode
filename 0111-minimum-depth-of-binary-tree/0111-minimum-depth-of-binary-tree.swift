e8bf0dc6db00e78f5c9b49531f9be31e4a44346d
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-07-10 13:39:44 -0700

    Time: 907 ms (82.44%), Space: 25.9 MB (17.56%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.swift
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.swift
@@ -1,16 +1,48 @@
-# Definition for a binary tree node.
-# class TreeNode
-#     attr_accessor :val, :left, :right
-#     def initialize(val = 0, left = nil, right = nil)
-#         @val = val
-#         @left = left
-#         @right = right
-#     end
-# end
-# @param {TreeNode} root
-# @return {Integer}
-def min_depth(root)
-    return 0 if !root
-    l,r = min_depth(root.left), min_depth(root.right)
-    1 + ((l==0 or r==0) ? l+r : [l,r].min) 
-end
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+ func minDepth(_ root: TreeNode?) -> Int {
+    guard let root = root else { return 0 }
+    var depth = 0
+    var queue = [root]
+    
+    while !queue.isEmpty {
+        var level = [TreeNode]()
+        depth += 1
+        
+        for node in queue {
+            if node.left == nil && node.right == nil {
+                return depth
+            }
+         
+            if let leftChild = node.left {
+                level.append(leftChild)
+
+            }
+            
+            if let rightChild = node.right {
+                level.append(rightChild)
+            }
+            
+        }
+        queue = level
+    }
+    
+    return depth
+    
+    }
+
+}
