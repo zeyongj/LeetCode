43011b429b3c3bfedb099afa492787b62ec268ca
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-07-10 13:38:52 -0700

    Time: 393 ms (19.05%), Space: 229 MB (30.95%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.rb
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.rb
@@ -1,34 +1,16 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} root
- * @return {number}
- */
-var minDepth = function(root) {
-    
-    if (root == null) return 0;
-    
-    let min = Number.MAX_SAFE_INTEGER;
-    
-    findMin(root, 1);
-    
-    return min;
-    
-    function findMin(node, depth) {
-        // base
-        if (node == null) return 0;
-        
-        if (node.left == null && node.right == null) {
-            min = Math.min(min, depth);
-        }
-        
-        findMin(node.left, depth + 1);
-        findMin(node.right, depth + 1);
-    }
-};
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Integer}
+def min_depth(root)
+    return 0 if !root
+    l,r = min_depth(root.left), min_depth(root.right)
+    1 + ((l==0 or r==0) ? l+r : [l,r].min) 
+end
