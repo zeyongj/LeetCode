8870f515d91f78e91efd4e6998c3e0281c13667a
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-07-10 13:42:46 -0700

    Time: 57 ms (9.52%), Space: 12.8 MB (90.48%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.rs
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.rs
@@ -1,25 +1,59 @@
-/**
- * Example:
- * var ti = TreeNode(5)
- * var v = ti.`val`
- * Definition for a binary tree node.
- * class TreeNode(var `val`: Int) {
- *     var left: TreeNode? = null
- *     var right: TreeNode? = null
- * }
- */
-class Solution {
-    fun minDepth(root: TreeNode?): Int = with(ArrayDeque<TreeNode>()) {
-      root?.let { add(it) }
-      generateSequence(1) { (it + 1).takeIf { isNotEmpty() } }
-        .firstOrNull {
-          (1..size).any {
-            with(poll()) {
-              left?.let { add(it) }
-              right?.let { add(it) }
-              left == null && right == null
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::collections::VecDeque;
+
+type Node = Rc<RefCell<TreeNode>>;
+
+//  Breath For Search 
+
+impl Solution {
+    pub fn min_depth(root: Option<Node>) -> i32 {
+
+        if root.is_none() { 
+            return 0 
+        }
+        let mut q = VecDeque::new();
+        //  append root node to q
+        //  (level, node)
+        q.push_back((1, root.clone()));
+
+        while !q.is_empty() { 
+            if let Some((level, Some(node))) =  q.pop_front() { 
+                let node = node.borrow();
+                //  Check whether a left or right node exist 
+                if node.left.is_none() && node.right.is_none() { 
+                    return level
+                } 
+					//**Can be optimised using if let syntax here
+                //  Add left node 
+                if node.left.is_some() { 
+                    q.push_back((level + 1, node.left.clone()));
+                } 
+                //  Add right node 
+                if node.right.is_some() {
+                    q.push_back((level + 1, node.right.clone()))
+
+                }
             }
-          }
-        } ?: 0
+        }
+      0
     }
 }
