314b6560d4d0484a26d84fab3bb7b6b85701723a
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-07-13 12:07:21 -0700

    Time: 19 ms (92.54%), Space: 7.2 MB (88.06%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.c
+++ 0207-course-schedule/0207-course-schedule.c
@@ -1,27 +1,50 @@
-from collections import defaultdict
+/* The most prerequisites a course has in the test data.  Used simply to cut down memory usage.  */
+#define PREREQ_MAX (13)
 
-class Solution:
-    def canFinish(self, numCourses: int, prerequisites: list[list[int]]) -> bool:
-        graph = defaultdict(list)
-        for x, y in prerequisites:
-            graph[x].append(y)
+bool loops( int idx, int cmap[][PREREQ_MAX], int cpr[], int *visited ) {
+    /* The current course requires a course that requires the 
+     * current course (looped requirements).
+     */
+    if ( visited[idx] > 0 )
+        return true;
+    
+    /* Check the current course's prerequisites for loops.  */
+    if ( visited[idx] == 0 ) {
+        visited[idx] = 1;
+        for ( int i=0; i<cpr[idx]; i++ ) {
+            if ( loops( cmap[idx][i], cmap, cpr, visited ) ) {
+                return true;
+            }
+        }
+        /* Mark course as processed in a previous loop.  Alternatively,
+         * can probably also just clear the visisted[] array.
+         */
+        visited[idx] = -1;
+    }
+    
+    return false;
+}
+
+bool canFinish(int numCourses, int** prerequisites, int prerequisitesSize, int* prerequisitesColSize){
+    int cmap[2001][PREREQ_MAX]; /* Holds the prerequisites of each course.  */
+    int cpr[2001]     = { 0 };  /* Number of course pre-requisites.  */
+    int visited[2001] = { 0 };
+
+    /* Map out all prerequisites of all courses.  */
+    for ( int i=0; i<prerequisitesSize; i++ ) {
+        int course = prerequisites[i][0];
+        int prereq = prerequisites[i][1];
         
-        visited = [0 for _ in range(numCourses)]
-        
-        def is_cyclic(i):
-            if visited[i] == -1:  # cycle detected
-                return True
-            if visited[i] == 1:  # this node is already checked and is safe
-                return False
-            
-            visited[i] = -1  # mark as being visited
-            for j in graph[i]:
-                if is_cyclic(j): 
-                    return True
-            visited[i] = 1  # mark as safe
-            return False
-        
-        for i in range(numCourses):
-            if is_cyclic(i):  # if a cycle is detected, return False
-                return False
-        return True
+        cmap[course][cpr[course]++] = prereq;
+    }
+    
+    /* Check for loops.  */
+    for ( int i=0; i<numCourses; i++ ) {
+	    /* If the courses loop, then we can't finish the courses.  */
+        if ( loops( i, cmap, cpr, &visited ) ) {
+            return false;
+        }
+    }
+    
+    return true;
+}
