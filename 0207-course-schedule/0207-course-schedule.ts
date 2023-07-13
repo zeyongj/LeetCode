7c6d9fe4c7a16f6ee2ff31bc3a5a05c44e52e4a7
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-07-13 12:28:07 -0700

    Time: 140 ms (13.76%), Space: 46.8 MB (98.14%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.ts
+++ 0207-course-schedule/0207-course-schedule.ts
@@ -1,59 +1,36 @@
-class Solution {
+function canFinish(numCourses: number, prerequisites: number[][]): boolean {
+    const preCourses: number[][] = []; 
+    const coursesTaken = new Set<number>();
 
-    /**
-     * @param Integer $numCourses
-     * @param Integer[][] $prerequisites
-     * @return Boolean
-     */
-    function canFinish($numCourses, $prerequisites) {
-        $adj = [];
-
-        for ($i = 0; $i < $numCourses; $i++) {
-            $adj[$i] = [];
-        }
-        
-        foreach ($prerequisites as $p) {
-            $adj[$p[0]][] = $p[1];
-        }
-
-        $nodeToVisitedState = [];
-        
-        for ($i = 0; $i < $numCourses; $i++) {
-            if ($this->detectCycleDFSRecursive($i, $adj, $nodeToVisitedState)) {
-                return false;
-            }
-        }
-
-        return true;
+    for(let i=0; i<numCourses; i++){
+        preCourses.push([]);
     }
 
+    for(const prerequisite of prerequisites){
+        const preCourse = prerequisite[1];
+        const course = prerequisite[0];
+        preCourses[course].push(preCourse);
+    }
 
-
-    function detectCycleDFSRecursive($currentNode, $adj, &$nodeToVisitedState) {
-        // Base cases
-        $currentVisitedState = isset($nodeToVisitedState[$currentNode]) ? $nodeToVisitedState[$currentNode] : 0;
-        // We're seeing a node again as part of one of its children recursive calls --> we have a cycle.
-        if ($currentVisitedState == 1) {
-            return true;
-        }
-        // We've already recursively explored all children of this node --> we can already determine that there is no cycle.
-        elseif ($currentVisitedState == 2) {
-            return false;
-        }
-    
-        // Mark the node as currently being recursively explored (but not complete again) so that we can identify if one of its children recursive calls touches it again (and thus that we have a cycle)
-        $nodeToVisitedState[$currentNode] = 1;
-    
-        foreach ($adj[$currentNode] as $neighbour) {
-            $neighbourHasCycle = $this->detectCycleDFSRecursive($neighbour, $adj, $nodeToVisitedState);
-            if ($neighbourHasCycle) {
-                return true;
+    let takeNewCourse = true;
+    while(takeNewCourse){
+        takeNewCourse = false;
+        for(let i=0; i<numCourses; i++){
+            if(coursesTaken.has(i)){
+                continue;
+            }
+            let canTake = true;
+            for(const preCourse of preCourses[i]){
+                if(!coursesTaken.has(preCourse)){
+                    canTake = false;
+                }
+            }
+            if(canTake){
+                coursesTaken.add(i)
+                takeNewCourse = true;
             }
         }
-    
-        // We're done recursively exploring its children - mark it as completely visited so we don't attempt to explore it again.
-        $nodeToVisitedState[$currentNode] = 2;
-        // We've explored the whole graph and haven't found a cycle!
-        return false;
     }
-}
+
+    return coursesTaken.size===numCourses;
+};
