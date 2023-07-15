f6d8d4433484c9b69dc83e089049207889b0038d
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-07-15 09:21:28 -0700

    Time: 187 ms (42.86%), Space: 36.5 MB (42.86%) - LeetHub

--- 0065-valid-number/0065-valid-number.kt
+++ 0065-valid-number/0065-valid-number.kt
@@ -1,10 +1,35 @@
-import scala.util.{Failure, Success, Try}
-object Solution {
-    def isNumber(s: String): Boolean = {
-        Try(s.toDouble) match {
-          case Success(_) =>
-            s.filter(_.isLetter).forall(_.toLower == 'e')
-          case Failure(_) => false
+class Solution {
+    fun isNumber(s: String): Boolean {
+        val finals = 0b101101000
+        val transfer = arrayOf(
+            intArrayOf(0, 1, 6, 2, -1),
+            intArrayOf(-1, -1, 6, 2, -1),
+            intArrayOf(-1, -1, 3, -1, -1),
+            intArrayOf(8, -1, 3, -1, 4),
+            intArrayOf(-1, 7, 5, -1, -1),
+            intArrayOf(8, -1, 5, -1, -1),
+            intArrayOf(8, -1, 6, 3, 4),
+            intArrayOf(-1, -1, 5, -1, -1),
+            intArrayOf(8, -1, -1, -1, -1)
+        )
+        var state = 0
+        for (c in s) {
+            val idx = make(c)
+            if (idx < 0) return false
+            state = transfer[state][idx]
+            if (state < 0) return false
+        }
+        return (finals and (1 shl state)) > 0
     }
-  }
+
+    private fun make(c: Char): Int {
+        return when (c) {
+            ' ' -> 0
+            '+', '-' -> 1
+            '.' -> 3
+            'e', 'E' -> 4
+            in '0'..'9' -> 2
+            else -> -1
+        }
+    }
 }
