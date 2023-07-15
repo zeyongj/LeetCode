55c4d3f67d1a046f2927c8d9b7048e7bce53aab0
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-07-15 09:24:08 -0700

    Time: 15 ms (50.00%), Space: 19.3 MB (33.33%) - LeetHub

--- 0065-valid-number/0065-valid-number.php
+++ 0065-valid-number/0065-valid-number.php
@@ -1,32 +1,45 @@
-impl Solution {
-    pub fn is_number(s: String) -> bool {
-        if let Some(e) = s.chars().position(|c| c == 'e' || c == 'E') {
-            Self::is_decimal(&s[0..e]) && Self::is_integer(&s[e + 1..])
-        } else {
-            Self::is_decimal(&s)
+class Solution {
+
+    /**
+     * @param String $s
+     * @return Boolean
+     */
+    function isNumber($s) {
+        $state = array_fill(0, 10, 0);
+        $finals = 0b101101000;
+        $transfer = [
+            [ 0, 1, 6, 2, -1],
+            [-1,-1, 6, 2, -1],
+            [-1,-1, 3, -1, -1],
+            [ 8,-1, 3, -1, 4],
+            [-1, 7, 5, -1, -1],
+            [ 8,-1, 5, -1, -1],
+            [ 8,-1, 6, 3, 4],
+            [-1,-1, 5, -1, -1],
+            [ 8,-1,-1, -1, -1]
+        ];
+        $idx = 0;
+        $chars = str_split($s);
+        foreach ($chars as $c) {
+            $idx = $this->make($c);
+            if ($idx < 0) return false;
+            $state[0] = $transfer[$state[0]][$idx];
+            if ($state[0] < 0) return false;
         }
+        return ($finals & (1 << $state[0])) > 0;
     }
-    fn is_decimal(s: &str) -> bool {
-        let (mut num, mut dot) = (false, false);
-        for (i, c) in s.chars().enumerate() {
-            match c {
-                '0'..='9' => num = true,
-                '+' | '-' if i == 0 => {}
-                '.' if !dot => dot = true,
-                _ => return false,
-            }
+
+    private function make($c) {
+        switch ($c) {
+            case ' ': return 0;
+            case '+': 
+            case '-': return 1;
+            case '.': return 3;
+            case 'e': 
+            case 'E': return 4;
+            default:
+                if (ctype_digit($c)) return 2;
+                return -1;
         }
-        num
-    }
-    fn is_integer(s: &str) -> bool {
-        let mut num = false;
-        for (i, c) in s.chars().enumerate() {
-            match c {
-                '0'..='9' => num = true,
-                '+' | '-' if i == 0 => {}
-                _ => return false,
-            }
-        }
-        num
     }
 }
