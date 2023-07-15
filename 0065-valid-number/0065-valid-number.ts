90d20be9daa6cc647876a93fe7d4eb06c95e84b7
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-07-15 09:25:21 -0700

    Time: 80 ms (61.22%), Space: 45.4 MB (28.57%) - LeetHub

--- 0065-valid-number/0065-valid-number.ts
+++ 0065-valid-number/0065-valid-number.ts
@@ -1,5 +1,35 @@
 function isNumber(s: string): boolean {
-    if(s.toLocaleLowerCase().includes('infinity')) return false;
-    const num: number = Number(s);
-    return !isNaN(num);
-};
+    let state = Array(10).fill(0);
+    const finals = 0b101101000;
+    const transfer = [
+        [ 0, 1, 6, 2, -1],
+        [-1,-1, 6, 2, -1],
+        [-1,-1, 3, -1, -1],
+        [ 8,-1, 3, -1, 4],
+        [-1, 7, 5, -1, -1],
+        [ 8,-1, 5, -1, -1],
+        [ 8,-1, 6, 3, 4],
+        [-1,-1, 5, -1, -1],
+        [ 8,-1,-1, -1, -1]
+    ];
+    let idx = 0;
+    for (let c of s) {
+        idx = make(c);
+        if (idx < 0) return false;
+        state[0] = transfer[state[0]][idx];
+        if (state[0] < 0) return false;
+    }
+    return (finals & (1 << state[0])) > 0;
+}
+
+function make(c: string): number {
+    switch (c) {
+        case ' ': return 0;
+        case '+':
+        case '-': return 1;
+        case '.': return 3;
+        case 'e':
+        case 'E': return 4;
+        default: return (c >= '0' && c <= '9') ? 2 : -1;
+    }
+}
