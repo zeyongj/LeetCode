18f392588b0fced489afedc90a53d5523c27ad5e
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-07-15 09:08:56 -0700

    Time: 78 ms (55.16%), Space: 44.6 MB (30.95%) - LeetHub

--- 0065-valid-number/0065-valid-number.js
+++ 0065-valid-number/0065-valid-number.js
@@ -1,36 +1,7 @@
-public class Solution {
-    public bool IsNumber(string s) {
-        // Exponential could be upper or lower case E / e, so change all
-        // characters to lower case. String is at least 1 character
-        s = s.ToLower();
-        bool isPeriod = false;
-        bool isExpo = false;
-        bool isNum = false;
-        for(int i = 0; i < s.Length; i++)
-        {
-            // check each character of the string s.
-            char checkChar = s[i];
-            if(checkChar >= '0' && checkChar <= '9') isNum = true;           
-                else if(checkChar == 'e')
-                {
-                    if(isExpo || !isNum)
-                    {
-                        return false;
-                    }                
-                isExpo = true;
-                isNum= false;
-            }
-            else if(checkChar == '.')
-            {
-                if(isPeriod || isExpo) return false;                
-                isPeriod = true;
-            }
-            else if(checkChar == '+' || checkChar == '-')
-            {               
-                if(i != 0 && s[i-1] != 'e') return false;               
-            }
-            else return false;           
-        }        
-        return isNum;
-    }
-}
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isNumber = function(s) {
+    return (! isNaN(s) ) && ( s.trim() !== "" ) && (! s.includes("Infinity") )
+};
