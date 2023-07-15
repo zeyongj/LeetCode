8e1836cde0640cdaef2419e667cd0412ff75a42b
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-07-15 09:08:29 -0700

    Time: 78 ms (81.90%), Space: 41.7 MB (52.38%) - LeetHub

--- 0065-valid-number/0065-valid-number.cs
+++ 0065-valid-number/0065-valid-number.cs
@@ -1,59 +1,36 @@
-/* Is the character a digit? */
-bool dig(char c)
-{
-    return (c >= '0' && c <= '9');
-}
-
-/*
- * Numbers 0-9
- * Exponent - "e"
- * Positive/negative sign - "+"/"-"
- * Decimal point - "."
- */
-bool isNumber(char *s) {    /* the string */
-    /* Get rid of these cases */
-    if (strlen(s) == 0) return 0;
-    if (!s) return 0;
-    
-    /* start parsing from beginning */
-    char *sp = s;
-    char deci = 0;
-    
-    /* What can we start with? */
-    while (*sp) if (*sp == ' ') sp++; else break;   /* spaces - eat up */
-    if (!*sp) return 0;                             /* all spaces => return false */
-    if (*sp == '+' || *sp == '-') *sp++;    /* +/- => allowed */
-    if (!*sp) return 0;                     /* nothing afterwards => return false */
-    if (*sp == '.') { deci = 1; sp++; }     /* '.' allowed for decimal */
-    if (!*sp) return 0;                     /* nothing afterwards => return false */
-    if (!dig(*sp++)) return 0;              /* not a digit? => return false */
-    
-    while (*sp) {
-        switch(*sp) {
-        case ' ':
-            sp++;
-            while (*sp) if (*sp == ' ') sp++; else break;
-            return (*sp == 0);              /* if we reached the end => trailing spaces */
-                
-        case '.':                           /* can't have two '.' => return false */
-            if (deci) return 0;
-            else deci = 1;
-            break;
-                
-        case 'e':                           /* parse the e or E case */
-        case 'E':
-            if (!*(sp + 1)) return 0; else sp++;
-            if (*sp == '+'|| *sp == '-') { if (!*(sp + 1)) return 0; else sp++; }
-            if (!dig(*sp)) return 0;
-            while (*sp) if (dig(*sp)) sp++; else break;
-            while (*sp) if (*sp == ' ') sp++; else break;
-            return (*sp == 0);
-                
-        default: 
-            if (!dig(*sp)) return 0;
-            break;
-        }
-        sp++;
+public class Solution {
+    public bool IsNumber(string s) {
+        // Exponential could be upper or lower case E / e, so change all
+        // characters to lower case. String is at least 1 character
+        s = s.ToLower();
+        bool isPeriod = false;
+        bool isExpo = false;
+        bool isNum = false;
+        for(int i = 0; i < s.Length; i++)
+        {
+            // check each character of the string s.
+            char checkChar = s[i];
+            if(checkChar >= '0' && checkChar <= '9') isNum = true;           
+                else if(checkChar == 'e')
+                {
+                    if(isExpo || !isNum)
+                    {
+                        return false;
+                    }                
+                isExpo = true;
+                isNum= false;
+            }
+            else if(checkChar == '.')
+            {
+                if(isPeriod || isExpo) return false;                
+                isPeriod = true;
+            }
+            else if(checkChar == '+' || checkChar == '-')
+            {               
+                if(i != 0 && s[i-1] != 'e') return false;               
+            }
+            else return false;           
+        }        
+        return isNum;
     }
-    return 1;
 }
