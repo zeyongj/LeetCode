18b4ba120c758afee83cf5d0733118bc6ea8ee32
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-07-15 08:59:12 -0700

    Time: 10 ms (9.51%), Space: 8.4 MB (5.27%) - LeetHub

---
+++ 0065-valid-number/0065-valid-number.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool isNumber(string s) {
+        int state = 0, finals = 0b101101000;
+        vector<vector<int>> transfer = {
+            { 0, 1, 6, 2, -1},
+            {-1,-1, 6, 2, -1},
+            {-1,-1, 3,-1,-1},
+            { 8,-1, 3,-1, 4},
+            {-1, 7, 5,-1,-1},
+            { 8,-1, 5,-1,-1},
+            { 8,-1, 6, 3, 4},
+            {-1,-1, 5,-1,-1},
+            { 8,-1,-1,-1,-1}
+        };
+        for (int i = 0; i < s.size(); ++i) {
+            int id = make(s[i]);
+            if (id < 0) return false;
+            state = transfer[state][id];
+            if (state < 0) return false;
+        }
+        return (finals & (1 << state)) > 0;
+    }
+
+private:
+    int make(char c) {
+        switch(c) {
+            case ' ' : return 0;
+            case '+':
+            case '-': return 1;
+            case '.': return 3;
+            case 'e':
+            case 'E': return 4;
+            default:
+                if (c >= 48 && c <= 57) return 2;
+        }
+        return -1;
+    }
+};
