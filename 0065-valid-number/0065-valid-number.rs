f887a8371a3247611de6e1afb3072144da5225d5
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-07-15 09:21:55 -0700

    Time: 0 ms (100.00%), Space: 2.1 MB (38.46%) - LeetHub

--- 0065-valid-number/0065-valid-number.rs
+++ 0065-valid-number/0065-valid-number.rs
@@ -1,35 +1,32 @@
-class Solution {
-    fun isNumber(s: String): Boolean {
-        val finals = 0b101101000
-        val transfer = arrayOf(
-            intArrayOf(0, 1, 6, 2, -1),
-            intArrayOf(-1, -1, 6, 2, -1),
-            intArrayOf(-1, -1, 3, -1, -1),
-            intArrayOf(8, -1, 3, -1, 4),
-            intArrayOf(-1, 7, 5, -1, -1),
-            intArrayOf(8, -1, 5, -1, -1),
-            intArrayOf(8, -1, 6, 3, 4),
-            intArrayOf(-1, -1, 5, -1, -1),
-            intArrayOf(8, -1, -1, -1, -1)
-        )
-        var state = 0
-        for (c in s) {
-            val idx = make(c)
-            if (idx < 0) return false
-            state = transfer[state][idx]
-            if (state < 0) return false
+impl Solution {
+    pub fn is_number(s: String) -> bool {
+        if let Some(e) = s.chars().position(|c| c == 'e' || c == 'E') {
+            Self::is_decimal(&s[0..e]) && Self::is_integer(&s[e + 1..])
+        } else {
+            Self::is_decimal(&s)
         }
-        return (finals and (1 shl state)) > 0
     }
-
-    private fun make(c: Char): Int {
-        return when (c) {
-            ' ' -> 0
-            '+', '-' -> 1
-            '.' -> 3
-            'e', 'E' -> 4
-            in '0'..'9' -> 2
-            else -> -1
+    fn is_decimal(s: &str) -> bool {
+        let (mut num, mut dot) = (false, false);
+        for (i, c) in s.chars().enumerate() {
+            match c {
+                '0'..='9' => num = true,
+                '+' | '-' if i == 0 => {}
+                '.' if !dot => dot = true,
+                _ => return false,
+            }
         }
+        num
+    }
+    fn is_integer(s: &str) -> bool {
+        let mut num = false;
+        for (i, c) in s.chars().enumerate() {
+            match c {
+                '0'..='9' => num = true,
+                '+' | '-' if i == 0 => {}
+                _ => return false,
+            }
+        }
+        num
     }
 }
