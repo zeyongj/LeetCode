d4adcbbe1007626e6e01e183c84f294f48d3f267
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-07-17 15:42:39 -0700

    Time: 20 ms (50.00%), Space: 18.9 MB (100.00%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.php
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.php
@@ -1,52 +1,52 @@
-// Definition for singly-linked list.
-// #[derive(PartialEq, Eq, Clone, Debug)]
-// pub struct ListNode {
-//   pub val: i32,
-//   pub next: Option<Box<ListNode>>
-// }
-// 
-// impl ListNode {
-//   #[inline]
-//   fn new(val: i32) -> Self {
-//     ListNode {
-//       next: None,
-//       val
-//     }
-//   }
-// }
-impl Solution {
-    pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-        let mut v1 = Solution::list2vec(&l1);
-        let mut v2 = Solution::list2vec(&l2);
-        v1.reverse();
-        v2.reverse();
-        let mut next = None;
-        let mut carry = 0;
-        for i in 0..=std::cmp::max(v1.len(), v2.len()) {
-            let mut val =
-                carry + if i < v1.len() { v1[i] } else { 0 } + if i < v2.len() { v2[i] } else { 0 };
-            carry = if val > 9 {
-                val -= 10;
-                1
-            } else {
-                0
-            };
-            if i < std::cmp::max(v1.len(), v2.len()) || val != 0 {
-                next = Some(Box::new(ListNode { val, next }));
-            }
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val = 0, $next = null) {
+ *         $this->val = $val;
+ *         $this->next = $next;
+ *     }
+ * }
+ */
+class Solution {
+    public function linkList($array1)
+    {
+        $array1 = array_values($array1);
+        $count = count($array1); 
+        $node = null;
+        for ($i = $count -1; $i > -1; $i--) {
+            $node = new ListNode($array1[$i], $node);
+        }     
+        
+        return $node;
+    } 
+    
+    /**
+     * @param ListNode $l1
+     * @param ListNode $l2
+     * @return ListNode
+     */    
+    function addTwoNumbers($l1, $l2) {
+        $current1 = $l1;
+        $num1 = '';
+        while ($current1->next) {
+            $num1 .= $current1->val;
+            $current1 = $current1->next;
         }
-        next
-    }
-    fn list2vec(list: &Option<Box<ListNode>>) -> Vec<i32> {
-        if list.is_none() {
-            return vec![0];
+        $num1 .= $current1->val;
+        
+        $current2 = $l2;
+        $num2 = '';
+        while ($current2->next) {
+            $num2 .= $current2->val;
+            $current2 = $current2->next;
         }
-        let mut ret = Vec::new();
-        let mut node = list;
-        while let Some(n) = node {
-            ret.push(n.val);
-            node = &n.next;
-        }
-        ret
+        $num2 .= $current2->val;
+        
+        $sum = bcadd($num2, $num1);    
+        $sum = str_split($sum);
+        
+        return $this->linkList($sum);
     }
 }
