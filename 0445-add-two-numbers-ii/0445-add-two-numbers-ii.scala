95ae42d7d70b3724c685254bdedd2e9a996c0467
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-07-17 15:41:07 -0700

    Time: 690 ms (25.00%), Space: 58.6 MB (100.00%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.scala
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.scala
@@ -1,36 +1,32 @@
 /**
  * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
+ * class ListNode(_x: Int = 0, _next: ListNode = null) {
+ *   var next: ListNode = _next
+ *   var x: Int = _x
  * }
  */
-func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-    var stack1, stack2 []int
-    for l1 != nil {
-        stack1 = append(stack1, l1.Val)
-        l1 = l1.Next
+object Solution {
+  import scala.collection.mutable
+  import scala.math.Integral.Implicits._
+  def addTwoNumbers(l1: ListNode, l2: ListNode): ListNode = {
+    val s1 = new mutable.Stack[Int]()
+    val s2 = new mutable.Stack[Int]()
+    def reverseList(l1: ListNode, s1: mutable.Stack[Int]): mutable.Stack[Int] = l1 match {
+      case null => s1
+      case l1 => { s1.push(l1.x)
+                   reverseList(l1.next, s1)}
     }
-    for l2 != nil {
-        stack2 = append(stack2, l2.Val)
-        l2 = l2.Next
+    reverseList(l1, s1)
+    reverseList(l2, s2)
+
+    def add(s1: mutable.Stack[Int], s2: mutable.Stack[Int], carry: Int, res: ListNode): ListNode = {
+      if (s1.isEmpty && s2.isEmpty && carry == 0) return res
+      val n1 = if (s1.nonEmpty) s1.pop() else 0
+      val n2 = if (s2.nonEmpty) s2.pop() else 0
+      val (q, r) = (carry + n1 + n2) /% 10
+      if (res == null)  add(s1, s2, q, new ListNode(r))
+      else add(s1, s2, q, new ListNode(r, res))
     }
-    var head *ListNode
-    var over int
-    for len(stack1) > 0 || len(stack2) > 0 || over > 0 {
-        sum := over
-        if len(stack1) > 0 {
-            sum += stack1[len(stack1)-1]
-            stack1 = stack1[:len(stack1)-1]
-        }
-        if len(stack2) > 0 {
-            sum += stack2[len(stack2)-1]
-            stack2 = stack2[:len(stack2)-1]
-        }
-        temp := ListNode{sum % 10, nil}
-        over = sum / 10
-        temp.Next = head
-        head = &temp
-    }
-    return head
+    add(s1, s2, 0, null)
+  }
 }
