aa09578f57e0129b954fa94722a9546335f353b8
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-07-17 15:38:44 -0700

    Time: 14 ms (70.77%), Space: 7.1 MB (84.62%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.c
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.c
@@ -1,31 +1,58 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        s1, s2 = [], []
-        
-        while l1:
-            s1.append(l1.val)
-            l1 = l1.next
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+static int addTwoNumInt(struct ListNode* l1, struct ListNode* l2, int l2missing) {
+	int carry, val;
+	
+	if (!l1)
+		return 0;
 
-        while l2:
-            s2.append(l2.val)
-            l2 = l2.next
-        
-        sum = 0
-        result = ListNode(0)
-        while s1 or s2:
-            if s1:
-                sum += s1.pop()
-            if s2:
-                sum += s2.pop()
-            result.val = sum % 10
-            head = ListNode(sum // 10)
-            head.next = result
-            result = head
-            sum //= 10
-            
-        return result.next if result.val == 0 else result
+	if (l2missing == 0) {
+		carry = addTwoNumInt(l1->next, l2->next, 0);
+		val = l1->val + l2->val + carry;
+	} else {
+		carry = addTwoNumInt(l1->next, l2, l2missing - 1);
+		val = l1->val + carry;
+	}
+	l1->val = val % 10;
+	return val / 10;
+	
+}
+
+static int length(struct ListNode* l) {
+    int len;
+    for (len = 0; l; l = l->next) 
+        len++;
+    return len;
+}
+
+static void swap(struct ListNode** p1, struct ListNode** p2) {
+    struct ListNode* tmp;
+    tmp = *p1;
+    *p1 = *p2;
+    *p2 = tmp;
+}
+
+#define abs(a) ((a) > 0 ? (a) : -(a))
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+	int len1, len2, carry;
+	struct ListNode* head;
+	len1 = length(l1);
+	len2 = length(l2);
+	if (len1 < len2)
+		swap(&l1, &l2);
+	carry = addTwoNumInt(l1, l2, abs(len1 - len2));
+	if (carry) {
+		if ((head = malloc(sizeof(struct ListNode))) == NULL)
+			return head;
+		head->val = 1;
+		head->next = l1;
+		l1 = head;
+	}
+	return l1;
+}
