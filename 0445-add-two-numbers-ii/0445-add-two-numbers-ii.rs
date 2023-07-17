2e15e5a1ef7fa988d7437e82d940eeb353a5922e
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-07-17 15:41:57 -0700

    Time: 4 ms (77.78%), Space: 2.1 MB (77.78%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.rs
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.rs
@@ -1,44 +1,52 @@
-/**
- * Example:
- * var li = ListNode(5)
- * var v = li.`val`
- * Definition for singly-linked list.
- * class ListNode(var `val`: Int) {
- *     var next: ListNode? = null
- * }
- */
-class Solution {
-    fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
-        if (l1 == null && l2 == null) return null
-        if (l1 == null && l2 != null) return l2
-        if (l1 != null && l2 == null) return l1
-        var q1: Stack<Int> = Stack()
-        var q2: Stack<Int> = Stack()
-        var (a, b) = l1 to l2
-        var sum: Int = 0
-        while (a != null) {
-            sum += a.`val`
-            q1.push(a.`val`)
-            a = a.next
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+// 
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut v1 = Solution::list2vec(&l1);
+        let mut v2 = Solution::list2vec(&l2);
+        v1.reverse();
+        v2.reverse();
+        let mut next = None;
+        let mut carry = 0;
+        for i in 0..=std::cmp::max(v1.len(), v2.len()) {
+            let mut val =
+                carry + if i < v1.len() { v1[i] } else { 0 } + if i < v2.len() { v2[i] } else { 0 };
+            carry = if val > 9 {
+                val -= 10;
+                1
+            } else {
+                0
+            };
+            if i < std::cmp::max(v1.len(), v2.len()) || val != 0 {
+                next = Some(Box::new(ListNode { val, next }));
+            }
         }
-        while (b != null) {
-            sum += b.`val`
-            q2.push(b.`val`)
-            b = b.next
+        next
+    }
+    fn list2vec(list: &Option<Box<ListNode>>) -> Vec<i32> {
+        if list.is_none() {
+            return vec![0];
         }
-        if (sum == 0) return ListNode(0)
-        var resultTail: ListNode? = null
-        var resultHead: ListNode? = null
-        var carry = 0
-        while (q1.isNotEmpty() || q2.isNotEmpty()) {
-            sum = carry
-            if (q1.isNotEmpty()) sum += q1.pop()
-            if (q2.isNotEmpty()) sum += q2.pop()
-            resultHead = ListNode(sum % 10).apply { next = resultTail }
-            resultTail = resultHead
-            carry = sum / 10
+        let mut ret = Vec::new();
+        let mut node = list;
+        while let Some(n) = node {
+            ret.push(n.val);
+            node = &n.next;
         }
-        if (carry != 0) resultHead = ListNode(carry).apply { next = resultTail }
-        return resultHead
+        ret
     }
 }
