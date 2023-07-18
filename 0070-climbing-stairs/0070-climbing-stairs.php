9f1366cc9132479c1cec66a6204ca74ce581308e
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-07-18 11:57:14 -0700

    Time: 8 ms (51.44%), Space: 19.2 MB (58.17%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.php
+++ 0070-climbing-stairs/0070-climbing-stairs.php
@@ -1,14 +1,21 @@
-impl Solution {
-    pub fn climb_stairs(n: i32) -> i32 {
-        let n = n as usize;
-        let mut dp = vec![1,2];
-        
-        for i in 2..n {
-            dp.push(
-                dp[i - 1] + dp[i - 2]
-            );
+class Solution {
+
+    /**
+     * @param Integer $n
+     * @return Integer
+     */
+    function climbStairs($n) {
+        if ($n == 1 || $n== 2){
+            return $n;
         }
-        
-        dp[n - 1]
+        $step1 = 1;
+        $step2 = 1;
+        $counter = 0;
+        for ($i = 0; $i < $n - 1; $i++) {
+            $counter = $step1 + $step2;
+            $step1 = $step2;
+            $step2 = $counter;
+        }
+        return $counter;
     }
 }
