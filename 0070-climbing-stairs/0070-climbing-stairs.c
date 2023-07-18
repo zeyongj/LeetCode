53d4e728e4ec7a62a3a8dcc288a52d7c6c7a2ceb
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-07-18 11:52:51 -0700

    Time: 0 ms (100.00%), Space: 5.7 MB (14.17%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.c
@@ -0,0 +1,14 @@
+int climbStairs(int n){
+        if (n <= 2) return n;
+        
+        int oneStepBefore = 2;
+        int twoStepsBefore = 1;
+        int allWays = 0;
+        
+        for (int i = 2; i < n; i++) {
+            allWays = oneStepBefore + twoStepsBefore;
+            twoStepsBefore = oneStepBefore;
+            oneStepBefore = allWays;
+        }
+        return allWays;
+}
