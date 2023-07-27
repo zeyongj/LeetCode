012a1bd0754df44ecfc4352d82b3e7c217129a6e
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-07-26 19:18:21 -0700

    Time: 53 ms (94.77%), Space: 43.1 MB (75.00%) - LeetHub

--- 0068-text-justification/0068-text-justification.ts
+++ 0068-text-justification/0068-text-justification.ts
@@ -1,78 +1,81 @@
-class Solution {
-
-    /**
-     * @param String[] $words
-     * @param Integer $maxWidth
-     * @return String[]
-     */
-    function fullJustify($words, $maxWidth) {
-        $output = [];
-        
-        $lineWidth = 0;
-        $wordsCount = 0;
-        $begin = 0;
-        $i = 0;
-        
-        while ($i < count($words)) {
-            $word = $words[$i];
-
-            $increase = strlen($word);
-            if ($i > $begin) {
-                $increase++;
-            }
-            
-			// find one line
-            if ($lineWidth + $increase <= $maxWidth) {
-                $lineWidth += $increase;
-                $i++;
-                $wordsCount++;
-                continue;
-            }
-            
-			// one line been found, append it to the output
-            if ($wordsCount == 1) {
-                $output[] = $words[$begin] . str_repeat(' ', $maxWidth - $lineWidth);
-            } else {
-                $gutter = intdiv($maxWidth - $lineWidth, $wordsCount - 1) + 1;
-                $extraGutter = ($maxWidth - $lineWidth) % ($wordsCount - 1);
-        
-                $aligned = '';
-
-                for ($j = $begin; $j < $i; $j++) {
-                    $aligned .= $words[$j];
-                    if ($j != $i - 1) {
-                        if ($extraGutter > 0) {
-                            $aligned .= str_repeat(' ', $gutter + 1);
-                            $extraGutter--;
-                        } else {
-                            $aligned .= str_repeat(' ', $gutter);
-                        }
-                    }
-                }
+function fullJustify(words: string[], maxWidth: number): string[] {
     
-                $output[] = $aligned;
-            }
-
-			// find the next line
-            $lineWidth = 0;
-            $wordsCount = 0;
-            $begin = $i;
+    type Payload = {
+        words: string[],
+        length: number,
+    };
+    
+    // Single responsibility to justtify a payload
+    // Padding length into the payload because we already have it to avoid recalculating
+    const justifyFromPayload = (payload: Payload): string => {
+        
+        let spacesToJustify = maxWidth - payload.length;
+        let spaceCount = payload.words.length - 1;
+        let justifiedText = '';
+        
+        // Loop through each word except the last one as we only add spaces after words 
+        //until the second last word
+        for(let i = 0; i < payload.words.length - 1; i++) {
+            
+            // Calculate the space needed for the current iteration
+            // This is the trickiest part of the problem for me
+            // If we have 5 spaces and we want to divide between 4 words
+            // it will be [word1]--[word2]--[word3]-[word4] 
+            // and NOT [word1]---[word2]-[word3]-[word4]
+            const spacePad = Math.ceil(spacesToJustify/spaceCount);
+            spacesToJustify -= spacePad;
+            spaceCount--;
+    
+            // Add the word and spaces needed after it        
+            justifiedText += payload.words[i] + ' '.repeat(Math.max(spacePad, 0));
         }
         
-		// append the last line
-        $aligned = '';
-
-        for ($j = $begin; $j < $i; $j++) {
-            $aligned .= $words[$j];
-            if ($j != $i - 1) {
-                $aligned .= ' ';
-            } else {
-                $aligned .= str_repeat(' ', $maxWidth - $lineWidth);
-            }
+        justifiedText += payload.words[payload.words.length - 1];
+        
+        return justifiedText.padEnd(maxWidth, ' ');
+    }
+    
+    // Justify last line
+    const justifyLastLine = (words: string[]) : string => {
+        return words.join(' ').padEnd(maxWidth, ' ');
+    }
+    
+    let answer: string[] = [];
+    // Iteration variables to keep track of the lengths and pass to the helper functions
+    let currentWords: string[] = [];
+    let currentLength: number = 0;
+    
+    // Loop through each word
+    words.forEach((word) => {
+        
+        // If by adding current word to the list of currentWords the length exceeds maxWidth
+        // Then pass the currentWords for justification
+        // Notice we also add spaces here
+        if(
+            currentLength // length of words so far
+            + word.length // The new word potentially getting added
+            + currentWords.length // spaces needed between word(as we need atleast 1 space between words)
+            > maxWidth) {
+            const newPayload: Payload = {
+                words: currentWords,
+                length: currentLength
+            };
+            answer.push(justifyFromPayload(newPayload));
+            // Reset the iteration variables as new justification payload
+            currentWords = [word];
+            currentLength = word.length;
+        }
+        else{
+            currentWords.push(word);
+            currentLength += word.length;
         }
         
-        $output[] = $aligned;
-
-        return $output;
-    }    
-}
+    });
+    
+    // If we have any words left, thats the last line
+    if(currentWords.length > 0) {
+        answer.push(justifyLastLine(currentWords));   
+    }
+    
+    return answer;
+};
