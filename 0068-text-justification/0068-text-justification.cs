585d15a2ba5570ac10410207d2e6569fe22e2d35
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-07-26 19:11:58 -0700

    Time: 170 ms (12.94%), Space: 43.5 MB (80.00%) - LeetHub

--- 0068-text-justification/0068-text-justification.cs
+++ 0068-text-justification/0068-text-justification.cs
@@ -1,95 +1,79 @@
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
-int getLen(char *s){
-    int len = 0;
-    while (s[len]) len++;
-    return len;
-}
-
-char *centerJustify(char **words, int wordsSize, int *len, int startIndex, int endIndex, int maxWidth){
-    int width = 0;
-    for (int i=startIndex; i<=endIndex; i++) width += len[i];
-    int totalSpace = maxWidth - width;
-    int space = totalSpace / (endIndex - startIndex);
-    int bonusSpace = totalSpace % (endIndex - startIndex);
-    char *line = (char*)malloc(sizeof(char)*(maxWidth + 1));
-    int index = 0;
-    for (int i=startIndex; i<=endIndex; i++){
-        // add space characters from the second word.
-        if (i > startIndex){
-            for (int j=0; j<space; j++) line[index++] = ' ';
-            if (bonusSpace-- > 0) line[index++] = ' ';
-        }
-        // add the word to the line
-        for (int j=0; j<len[i]; j++){
-            line[index++] = words[i][j];
-        }
-    }
-    line[index] = 0;
-    
-    return line;
-}
-
-char *leftJustify(char **words, int wordsSize, int *len, int startIndex, int endIndex, int maxWidth){
-    char *line = (char*)malloc(sizeof(char)*(maxWidth + 1));
-    int index = 0;
-    for (int i=startIndex; i<=endIndex; i++){
-        // add space characters from the second word.
-        if (i > startIndex) line[index++] = ' ';
-        // add the word to the line
-        for (int j=0; j<len[i]; j++){
-            line[index++] = words[i][j];
-        }
-    }
-    for(int i = index; i<maxWidth; i++) line[index++] = ' ';
-    line[index] = 0;
-    
-    return line;
-}
-
-char** fullJustify(char** words, int wordsSize, int maxWidth, int* returnSize) {
-    if (wordsSize == 0){
-		// return a line with all space charaters
-		char **out = (char**)malloc(sizeof(char*));
-		out[0] = (char*)malloc(sizeof(char) * maxWidth + 1);
-		int index = 0;
-		for (int i=0; i<maxWidth; i++) out[0][index++] = ' ';
-		out[0][index] = 0;
-		*returnSize = 1;
-		return out;
-	}
-    int *len = (int*)calloc(wordsSize, sizeof(int));
-    for (int i=0; i<wordsSize; i++){
-        len[i] = getLen(words[i]);
-    }
+public class Solution {
+    public IList<string> FullJustify(string[] words, int maxWidth) {
+        if (words == null || words.Length == 0)
+            return new List<string>();
         
-    int *startIndex = (int*)malloc(sizeof(int) * wordsSize);
-    int startLen = 0;
-    int width = 0, newWidth;
-    startIndex[startLen++] = 0;
-    for (int i=0; i<wordsSize; i++){
-        newWidth = width + len[i];
-        if (width > 0) newWidth++;
+        IList<string> res = new List<string>();
         
-        if (newWidth > maxWidth){
-        	startIndex[startLen++] = i;
-        	width = len[i];
-		}
-        else width = newWidth;
-    }
+        for (int i = 0; i < words.Length; i++)
+        {
+                StringBuilder cur = new StringBuilder();
+                int j = i,
+                    len = 0;
+
+                while (i < words.Length && len + words[i].Length + i - j <= maxWidth)
+                    len += words[i++].Length;
+
+                i--;
+
+                if (j == i)
+                {
+                    int k = maxWidth - words[j].Length;
+
+                    cur.Append(words[j]);
+
+                    while (k-- > 0)
+                        cur.Append(" ");
+                }
+                else if (i == words.Length - 1)
+                {
+                    int k = maxWidth;
+
+                    while (j <= i)
+                    {
+                        cur.Append(words[j] + " ");
+                        k -= words[j].Length + 1;
+                        j++;
+                    }
+                    
+                    cur = new StringBuilder(cur.ToString().Trim());
+                    k++;
+                    
+                    while (k-- > 0)
+                        cur.Append(" ");
+                }
+                else
+                {
+                    int baseCnt = (maxWidth - len) / (i - j),
+                        cnt = (maxWidth - len) % (i - j);
+
+                    if (cnt != 0)
+                        while (cnt-- > 0)
+                        {
+                            int k = 0;
+
+                            cur.Append(words[j++]);
+
+                            while (k++ < baseCnt + 1)
+                                cur.Append(" ");
+                        }
+
+                    while (j < i)
+                    {
+                        int k = 0;
+
+                        cur.Append(words[j++]);
+
+                        while (k++ < baseCnt)
+                            cur.Append(" ");
+                    }
+
+                    cur.Append(words[j]);
+                }
+
+                res.Add(cur.ToString());
+        }
         
-    char **out = (char**)malloc(sizeof(char*) * startLen);
-    int index = 0;
-    for (int i=0; i<startLen - 1; i++){
-    	if (startIndex[i + 1] - startIndex[i] >= 2)
-        	out[index++] = centerJustify(words, wordsSize, len, startIndex[i], startIndex[i + 1] - 1, maxWidth);
-        else
-        	out[index++] = leftJustify(words, wordsSize, len, startIndex[i], startIndex[i + 1] - 1, maxWidth);
+        return res;
     }
-    out[index] = leftJustify(words, wordsSize, len, startIndex[startLen - 1], wordsSize - 1, maxWidth);
-    
-    free(startIndex);
-    *returnSize = startLen;
-    return out;
 }
