c0f91ef5fa8a24b4eb1ca188a29546dda309c29f
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-07-26 19:15:42 -0700

    Time: 168 ms (74.39%), Space: 37.3 MB (60.98%) - LeetHub

--- 0068-text-justification/0068-text-justification.kt
+++ 0068-text-justification/0068-text-justification.kt
@@ -1,53 +1,66 @@
-var sb strings.Builder
+class Solution {
+    private  val SEPARATOR = " " 
+    
+    fun fullJustify(words: Array<String>, maxWidth: Int): List<String> {
 
-func fullJustify(words []string, maxWidth int) []string {
-	var start, end, runeCount int
-	var result = make([]string, 0, len(words))
+        val result = mutableListOf<String>() // keeps the lines as a sting in a paragraph
+        val currentLineWords = mutableListOf<String>()// keeps track of all the words for the current line
+        var availableSpacePerLine = maxWidth //  keeps track of the available space in the current line
 
-	for start < len(words) {
-		start = end
-		runeCount = 0
-		for end < len(words) && runeCount+end-start+len(words[end]) <= maxWidth {
-			runeCount += len(words[end])
-			end++
-		}
-		if start == end {
-			break
-		}
-		if end == len(words) {
-		// last line left justify
-		str := strings.Join(words[start:end], " ")
-			result = append(result, str+strings.Repeat(" ", maxWidth-len(str)))
-		} else {
-			result = append(result, generateString(words[start:end], runeCount, maxWidth))
-		}
-	}
+        words.forEach { word ->
+            availableSpacePerLine -= word.length
+            when {
+                (availableSpacePerLine == 0) -> { // the words perfectly fit
+                    currentLineWords.add(word)
+                    result.add(toLineString(availableSpacePerLine, currentLineWords))
 
-	return result
-}
+                    // Start a new line
+                    currentLineWords.clear()
+                    availableSpacePerLine = maxWidth
+                }
+                (availableSpacePerLine < 0) -> { // too much words in a line adjust!
+                    availableSpacePerLine += (word.length + 1) //remove the claimed space for current
+                    result.add(toLineString(availableSpacePerLine, currentLineWords))
 
-func generateString(words []string, runeCount, maxWidth int) string {
-	sb.Reset()
-	var spaceCount, extraSpaces int
-	if len(words) > 1 {
-		spaceCount = (maxWidth - runeCount) / (len(words) - 1)
-		extraSpaces = (maxWidth - runeCount) % (len(words) - 1)
-	}
+                    // Start a new line
+                    currentLineWords.clear()
+                    currentLineWords.add(word)
+                    availableSpacePerLine = maxWidth - (word.length + 1)
+                }
+                (availableSpacePerLine > 0) -> { // space is still available in the current line
+                    currentLineWords.add(word)
+                    availableSpacePerLine--
+                }
+            }
+        }
 
-	for i := 0; i < len(words)-1; i++ {
-		sb.WriteString(words[i])
-		sb.WriteString(strings.Repeat(" ", spaceCount))
-		if extraSpaces > 0 {
-			sb.WriteString(" ")
-			extraSpaces--
-		}
-	}
+        // Process the last line if there is one
+        if (currentLineWords.isNotEmpty()) {
+            result.add(toLineString(availableSpacePerLine + 1, currentLineWords, true))
+        }
+        return result
+    }
+    
 
-	sb.WriteString(words[len(words)-1])
-
-	if sb.Len() < maxWidth {
-		sb.WriteString(strings.Repeat(" ", maxWidth-sb.Len()))
-	}
-
-	return sb.String()[:maxWidth]
+    private fun toLineString(
+        noOfSpaceToBeDistributed: Int,
+        wordsInLine: MutableList<String>,
+        isLastLine: Boolean = false
+    ): String {
+        return if (wordsInLine.size == 1 || isLastLine) { // if there is only one word in a line or if the line is the last one , all the remaining spaces should just go to the end of the sentence
+            wordsInLine.joinToString(SEPARATOR) + SEPARATOR.repeat(noOfSpaceToBeDistributed)
+        } else { // other wise we have to evenly distribute the remaining lines
+            val spaceToBeAddedToAllWords = (Math.floorDiv(
+                noOfSpaceToBeDistributed,
+                wordsInLine.lastIndex
+            )) + 1 // the +1 is because we already take in to consideration a space when calculating noOfSpaceToBeDistributed
+            
+            for (i in 0 until noOfSpaceToBeDistributed % wordsInLine.lastIndex) {
+                wordsInLine[i] += SEPARATOR
+            }
+            wordsInLine.joinToString(SEPARATOR.repeat(spaceToBeAddedToAllWords))
+        }
+    }
+  
+  
 }
