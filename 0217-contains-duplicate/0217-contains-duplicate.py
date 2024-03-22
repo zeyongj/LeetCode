dc60f082f918edbb9cf960b74739d93cda35b828
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2024-03-22 16:44:41 -0700

    Time: 465 ms (5.80%), Space: 34.5 MB (23.80%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -11,9 +11,27 @@ class Solution:
             if num not in numDict.keys():
                 numDict[num] = 1
             else:
+                val = numDict.get(num)
+                val += 1
+                numDict[num] = val
+        
+        occurances = numDict.values()
+        
+        for occurance in occurances:
+            if occurance != 1:
                 return True
         
         return False
+        
+#         for num in nums:
+#             if num not in numDict.keys():
+#                 numDict[num] = 1
+#             else:
+#                 return True
+            
+#         return False
+
+
         
 #         mySet = set()
         
