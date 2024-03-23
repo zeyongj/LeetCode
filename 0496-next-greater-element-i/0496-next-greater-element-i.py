1197ecc0aa58a68aa0748eead110c181fb69770d
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2024-03-23 00:50:41 -0700

    Time: 54 ms (37.48%), Space: 16.8 MB (45.21%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -3,28 +3,49 @@ class Solution:
         output = []
         
         stack = []
+        dictionary = {}
         
         for num in nums2:
+            while (len(stack) > 0 and stack[-1] < num):
+                temp = stack.pop()
+                dictionary.update({temp:num})
             stack.append(num)
-            
+        
+        # Remaining assigned -1
+        while (len(stack) != 0):
+            temp = stack.pop()
+            dictionary.update({temp:-1})
+        
         for num in nums1:
-            temp = []
-            isFound = False
-            max = -1
-            while (len(stack) != 0 and isFound == False):
-                top = stack.pop()
-                temp.append(top)
-                if top > num:
-                    max = top
-                elif top == num:
-                    isFound = True
-                else:
-                    continue
-            output.append(max)
-            
-            while (len(temp) != 0):
-                stack.append(temp.pop())
+            output.append(dictionary.get(num))
         
         return output
+        
+#         output = []
+        
+#         stack = []
+        
+#         for num in nums2:
+#             stack.append(num)
+            
+#         for num in nums1:
+#             temp = []
+#             isFound = False
+#             max = -1
+#             while (len(stack) != 0 and isFound == False):
+#                 top = stack.pop()
+#                 temp.append(top)
+#                 if top > num:
+#                     max = top
+#                 elif top == num:
+#                     isFound = True
+#                 else:
+#                     continue
+#             output.append(max)
+            
+#             while (len(temp) != 0):
+#                 stack.append(temp.pop())
+        
+#         return output
                 
                     
