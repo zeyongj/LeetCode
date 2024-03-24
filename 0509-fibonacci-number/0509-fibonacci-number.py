57ff5463d85dbed232bb6246502a9ad7f27e3262
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2024-03-23 21:17:16 -0700

    Time: 353 ms (21.44%), Space: 16.4 MB (83.56%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -3,6 +3,6 @@ class Solution:
         if n < 2:
             return n
         
-        sum = self.fib(n-1) + self.fib(n-2)
+        sum = self.fib(n-1) + self.fib(n-2) # # Use self to refer to the current instance of the class.
         
         return sum
