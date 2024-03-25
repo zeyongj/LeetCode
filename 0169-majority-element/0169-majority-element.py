2a372c022115ce2b1cec7d221fec3ca77399a9ce
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2024-03-25 15:31:20 -0700

    Time: 204 ms (5.01%), Space: 17.9 MB (78.12%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,15 +1,45 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        num_map = {}
+        # Divide and Conquer Method:
+        return self.getMajority(nums, 0, len(nums)-1)
+    def getMajority(self, nums, left, right) -> int:
+        if left == right:
+            return nums[left]
         
-        n = len(nums)
+        mid = (left + right)//2
         
-        for num in nums:
-            if num not in num_map.keys():
-                num_map[num] = 1
-            else:
-                num_map[num] += 1
+        leftMajor = self.getMajority(nums, left, mid)
+        rightMajor = self.getMajority(nums, mid+1, right)
+        
+        if leftMajor == rightMajor:
+            return leftMajor
+        
+        leftCount = 0
+        rightCount = 0
+        
+        for i in range(left,right+1):
+            if nums[i] == leftMajor:
+                leftCount += 1
+            elif nums[i] == rightMajor:
+                rightCount += 1
+        
+        if leftCount > rightCount:
+            return leftMajor
+        else:
+            return rightMajor
+                    
+        
+#         HashMap Method:
+#         num_map = {}
+        
+#         n = len(nums)
+        
+#         for num in nums:
+#             if num not in num_map.keys():
+#                 num_map[num] = 1
+#             else:
+#                 num_map[num] += 1
                 
-        for key,value in num_map.items():
-            if value > n/2:
-                return key
+#         for key,value in num_map.items():
+#             if value > n/2:
+#                 return key
