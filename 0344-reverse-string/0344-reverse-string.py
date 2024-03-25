3d001ec2d08b6e88241d5148d7349ae260b4d1bb
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2024-03-24 19:47:32 -0700

    Time: 168 ms (66.83%), Space: 20.9 MB (46.04%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -8,14 +8,31 @@ class Solution:
         if size == 0:
             return []
         
-        left = 0
-        right = size - 1
+        temp = []
         
-        while left < right:
-            temp = s[left]
-            s[left] = s[right]
-            s[right] = temp
-            left += 1
-            right -= 1
+        for c in s:
+            temp.append(c)
+            
+        for i in range(size):
+            s[i] = temp.pop()
+        
+        
+            
+        
+        # Double pointer swap:
+#         size = len(s)
+        
+#         if size == 0:
+#             return []
+        
+#         left = 0
+#         right = size - 1
+        
+#         while left < right:
+#             temp = s[left]
+#             s[left] = s[right]
+#             s[right] = temp
+#             left += 1
+#             right -= 1
             
         
