54c44fdce9c00bf3c4939102a7727bfd2733f061
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2024-03-24 23:01:20 -0700

    Time: 41 ms (27.78%), Space: 16.7 MB (78.32%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,23 +1,48 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = [[]]
+        # DFS Method:
         
-        size = len(nums)
+        res = []
         
-        for i in range (1, size + 1):
-            self.backtracking(nums, res, i, 0, [])
+        self.dfs(nums, res, 0, [])
         
         return res
-    
-    def backtracking(self, nums: List[int], result: List[List[int]], length: int, index: int, subset: List[int]) -> None:
-        if len(subset) == length:
-            temp = subset.copy()
-            result.append(temp)
-            
+        
+    def dfs(self, nums: List[int], result: List[List[int]], index: int, subset: List[int]) -> None:
+        temp = subset.copy()
+        result.append(temp)
+        
+        if index == len(nums):
+            return
+        
         for i in range(index, len(nums)):
             subset.append(nums[i])
-            self.backtracking(nums, result, length, i+1, subset)
+            self.dfs(nums, result, i+1, subset)
             subset.pop()
+            
+        
+        
+            
+        
+#         # Backtracking Method:
+#         res = [[]]
+        
+#         size = len(nums)
+        
+#         for i in range (1, size + 1):
+#             self.backtracking(nums, res, i, 0, [])
+        
+#         return res
+    
+#     def backtracking(self, nums: List[int], result: List[List[int]], length: int, index: int, subset: List[int]) -> None:
+#         if len(subset) == length:
+#             temp = subset.copy()
+#             result.append(temp)
+            
+#         for i in range(index, len(nums)):
+#             subset.append(nums[i])
+#             self.backtracking(nums, result, length, i+1, subset)
+#             subset.pop()
             
     
     
