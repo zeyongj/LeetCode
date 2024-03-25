87491c6e200d6e20eb761110bfef4490b2d7de52
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2024-03-24 19:58:20 -0700

    Time: 165 ms (79.72%), Space: 23 MB (5.22%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -8,13 +8,35 @@ class Solution:
         if size == 0:
             return []
         
-        temp = []
+        left = 0
+        right = size - 1
         
-        for c in s:
-            temp.append(c)
+        self.recursion(s, left, right)
+        
+    def recursion(self, s: List[str], left: int, right: int) -> None:
+        if left >= right:
+            return []
+        
+        self.recursion(s, left+1, right-1)
+        
+        temp = s[left]
+        s[left] = s[right]
+        s[right] = temp
+    
+        
+#         # Stack method:
+#         size = len(s)
+        
+#         if size == 0:
+#             return []
+        
+#         temp = []
+        
+#         for c in s:
+#             temp.append(c)
             
-        for i in range(size):
-            s[i] = temp.pop()
+#         for i in range(size):
+#             s[i] = temp.pop()
         
         
             
