6a640168236e7287632d7864c4bbb69a4575a192
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2024-03-24 22:28:10 -0700

    Time: 36 ms (63.27%), Space: 16.9 MB (8.19%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -2,13 +2,37 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = [[]]
         
-        for num in nums:
-            extension = [set + [num] for set in res]
-            res.extend(extension)
-            # temp = []
-            # for set in res:
-            #     extension = set + [num]
-            #     temp.append(extension)
-            # res.extend(temp)
+        size = len(nums)
+        
+        for i in range (1, size + 1):
+            self.backtracking(nums, res, i, 0, [])
         
         return res
+    
+    def backtracking(self, nums: List[int], result: List[List[int]], length: int, index: int, subset: List[int]) -> None:
+        if len(subset) == length:
+            temp = subset.copy()
+            result.append(temp)
+            
+        for i in range(index, len(nums)):
+            subset.append(nums[i])
+            self.backtracking(nums, result, length, i+1, subset)
+            subset.pop()
+            
+    
+    
+            
+        
+#         # Extension Method:
+#         res = [[]]
+        
+#         for num in nums:
+#             extension = [set + [num] for set in res]
+#             res.extend(extension)
+#             # temp = []
+#             # for set in res:
+#             #     extension = set + [num]
+#             #     temp.append(extension)
+#             # res.extend(temp)
+        
+#         return res
