f48106feb910eca8b0b408e8a5586b1e4d4338fd
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2024-03-25 02:55:54 -0700

    Time: 25 ms (97.71%), Space: 16.3 MB (98.89%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,18 +1,33 @@
 class Solution:
-    # Memorization Mechanism
-    def __init__(self):
-        self.memo = {}
-    
+    # Dynamic Programming:
     def fib(self, n: int) -> int:
-        if n < 2 :
-            self.memo[n] = n
+        if n < 2:
+            return n
         
-        if n in self.memo.keys():
-            return self.memo[n]
-        else:
-            self.memo[n] = self.fib(n-1) + self.fib(n-2) # # Use self to refer to the current instance of the class.
+        dp = [0]*(n+1)
         
-        return self.memo[n]
+        dp[0] = 0
+        dp[1] = 1
+        
+        for i in range(2,n+1):
+            dp[i] = dp[i-2] + dp[i-1]
+        
+        return dp[n]
+    
+#     # Memorization Mechanism
+#     def __init__(self):
+#         self.memo = {}
+    
+#     def fib(self, n: int) -> int:
+#         if n < 2 :
+#             self.memo[n] = n
+        
+#         if n in self.memo.keys():
+#             return self.memo[n]
+#         else:
+#             self.memo[n] = self.fib(n-1) + self.fib(n-2) # # Use self to refer to the current instance of the class.
+        
+#         return self.memo[n]
     
     # Brutal Force:
 #     def fib(self, n: int) -> int:
