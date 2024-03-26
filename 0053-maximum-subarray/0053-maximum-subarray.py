0f0c7a77dc8399a0e53d6bff3bc5e99a2aa59bff
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2024-03-25 17:21:01 -0700

    Time: 1536 ms (5.06%), Space: 31.2 MB (17.04%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -2,39 +2,40 @@ class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         size = len(nums)
         
-        # Use self. to call the method within the class
         return self.getMax(nums, 0, size - 1)
     
     def getMax(self, nums: List[int], left: int, right: int) -> int:
+        size = len(nums)
         if left == right:
             return nums[left]
         
-        mid = (left + right) // 2
+        mid = (left+right) // 2
         
         leftMax = self.getMax(nums, left, mid)
-        rightMax = self.getMax(nums, mid + 1, right)
-        middleMax = self.getMidMax(nums, left, mid, right)
+        rightMax = self.getMax(nums, mid+1, right)
+        middleMax = self.getMidMax(nums, left, right)
         
         return max(leftMax, rightMax, middleMax)
     
-    def getMidMax(self, nums: List[int], left: int, mid: int, right: int) -> int:
-        if left == right:
-            return nums[left]
+    def getMidMax(self, nums: List[int], left: int, right: int) -> int:
+        size = len(nums)
+        mid = (left+right) // 2
         
-        leftSum = 0
-        leftMax = float('-inf')
+        leftSum = nums[mid]
+        leftMax = leftSum
         
-        for i in range(mid, left - 1, -1):
+        for i in range(mid-1, left-1, -1):
             leftSum += nums[i]
             leftMax = max(leftMax, leftSum)
             
-        rightSum = 0
-        rightMax = float('-inf')
+        rightSum = nums[mid+1]
+        rightMax = rightSum
         
-        for i in range(mid + 1, right + 1):
+        for i in range(mid+2, right+1):
             rightSum += nums[i]
             rightMax = max(rightMax, rightSum)
         
-        # Include nums[mid] in the sum across the middle
-        return leftMax + rightMax
-
+        middleMax = leftMax + rightMax
+        return middleMax
+            
+        
