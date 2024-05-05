8459b962c2508a0c1b58cb32f9e3a9a38441aec8
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2024-05-05 15:37:57 -0700

    Time: 36 ms (60.15%), Space: 16.5 MB (37.80%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.py
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.py
@@ -6,13 +6,16 @@ class Solution:
             return 0
         
         ans = 0
-        # digit = 0
-        # while digit < size:
-        #     ans = ans * 26 + (ord(columnTitle[digit]) - 64)
-        #     digit += 1
+        index = 0
+        while index < size:
+            power = size - index - 1
+            ans += (26 ** power) * (ord(columnTitle[index]) - 64)
+            index += 1
         
-        for digit, char in enumerate(reversed(columnTitle)):
-            num = ord(char) - 64
-            ans += num * (26 ** digit)
+        
+        
+        # for digit, char in enumerate(reversed(columnTitle)):
+        #     num = ord(char) - 64
+        #     ans += num * (26 ** digit)
         
         return ans
