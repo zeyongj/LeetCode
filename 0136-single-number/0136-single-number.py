e88c98578a23abd7156e726348c574fa73756b95
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2024-05-07 12:40:54 -0700

    Time: 111 ms (55.81%), Space: 19.1 MB (33.87%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,15 +1,24 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        mapping = {}
+        ans = 0
         
         for num in nums:
-            if num in mapping.keys():
-                mapping[num] += 1
-            else:
-                mapping[num] = 1
+            ans = ans ^ num
         
-        for num, occur in mapping.items():
-            if occur == 1:
-                return num
+        return ans
+        
+#         mapping = {}
+        
+#         for num in nums:
+#             if num in mapping.keys():
+#                 mapping[num] += 1
+#             else:
+#                 mapping[num] = 1
+        
+#         for num, occur in mapping.items():
+#             if occur == 1:
+#                 return num
+
+    
                 
         
