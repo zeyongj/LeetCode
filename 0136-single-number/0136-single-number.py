ef98cecc555bde32be2738e38b8e874177968782
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2024-05-07 12:44:47 -0700

    Time: 102 ms (91.07%), Space: 19.2 MB (33.87%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,11 +1,15 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        
+        # Xor Method - Time Complexity: O(n), Space Complexity: O(1)
         ans = 0
         
         for num in nums:
-            ans = ans ^ num
+            ans = ans ^ num # xor property: x^x = 0 -> only single number remains, others are cancelled
         
         return ans
+    
+        # HashMap Mathod - Time Complexity: O(n), Space Complexity: O(1)
         
 #         mapping = {}
         
