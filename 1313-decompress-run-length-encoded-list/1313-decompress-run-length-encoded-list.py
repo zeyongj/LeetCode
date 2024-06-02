e58ee1972ede6eea3afb74e039ed9dd71963551f
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2024-06-02 15:29:19 -0700

    Time: 56 ms (42.93%), Space: 16.9 MB (93.44%) - LeetHub

--- 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.py
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.py
@@ -1,6 +1,8 @@
 class Solution:
     def decompressRLElist(self, nums: List[int]) -> List[int]:
-        return reduce(
-            lambda x, y: x + y,
-            (f * [v] for f, v in list(zip(*([iter(nums)] * 2)))),
-        )
+        generated = []
+        for i in range(0, len(nums), 2):
+            freq = nums[i]
+            val = nums[i+1]
+            generated += [val] * freq 
+        return generated
