5c95125d3ab071e179c27d38c29d91f716692f81
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2024-06-08 13:37:37 -0700

    Time: 57 ms (59.43%), Space: 16.6 MB (77.10%) - LeetHub

---
+++ 1370-increasing-decreasing-string/1370-increasing-decreasing-string.py
@@ -0,0 +1,18 @@
+class Solution:
+    def sortString(self, s: str) -> str:
+        letter_set = set(s)
+        freq = {}
+        ans = ''
+        ref = 'abcdefghijklmnopqrstuvwxyz'
+        for i in letter_set:
+            freq[i] = s.count(i)
+        while len(ans) != len(s):
+            for i in ref:
+                if i in freq and freq[i] > 0:
+                    ans += i
+                    freq[i] -= 1
+            for i in ref[::-1]:
+                if i in freq and freq[i] > 0:
+                    ans += i
+                    freq[i] -= 1
+        return ans
