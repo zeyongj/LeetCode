b3e6d8d4df00ebe2e12d7fe960c02181e8efbec2
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2024-06-08 17:48:27 -0700

    Time: 10 ms (6.02%), Space: 49.4 MB (53.01%) - LeetHub

---
+++ 0335-self-crossing/0335-self-crossing.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public boolean isSelfCrossing(int[] x) {
+        int l = x.length;
+        if(l <= 3) return false;
+        
+        for(int i = 3; i < l; i++){
+            if(x[i] >= x[i-2] && x[i-1] <= x[i-3]) return true;  //Fourth line crosses first line and onward
+            if(i >=4)
+            {
+                if(x[i-1] == x[i-3] && x[i] + x[i-4] >= x[i-2]) return true; // Fifth line meets first line and onward
+            }
+            if(i >=5)
+            {
+                if(x[i-2] - x[i-4] >= 0 && x[i] >= x[i-2] - x[i-4] && x[i-1] >= x[i-3] - x[i-5] && x[i-1] <= x[i-3]) return true;  // Sixth line crosses first line and onward
+            }
+        }
+        return false;
+    }
+}
