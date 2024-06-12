fcb28055a33b028155b9d2a8c97884bc2442ce14
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2024-06-12 12:06:53 -0700

    Time: 336 ms (29.49%), Space: 16.7 MB (83.33%) - LeetHub

---
+++ 2763-sum-of-imbalance-numbers-of-all-subarrays/2763-sum-of-imbalance-numbers-of-all-subarrays.py
@@ -0,0 +1,23 @@
+class Solution:
+    def sumImbalanceNumbers(self, nums: List[int]) -> int:
+        
+        ans = 0
+        
+        for i,start in enumerate(nums):
+            d = {}
+            d[start] = 1
+            cnt = 0
+            
+            for j in range(i+1, len(nums)):
+                n = nums[j]
+                if n in d:
+                    pass
+                else:
+                    if n+1 in d and n-1 in d:
+                        cnt -= 1
+                    elif n+1 not in d and n-1 not in d:
+                        cnt += 1
+                d[n] = 1
+                ans += cnt
+        
+        return ans
