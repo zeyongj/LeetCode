51bedeff87892128f072e3b87c6df922b75f3734
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2024-06-17 12:07:46 -0700

    Time: 38 ms (99.89%), Space: 16.4 MB (96.81%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def judgeSquareSum(self, c):
+        divisor = 2
+        while divisor * divisor <= c:
+            if c % divisor == 0:
+                exponentCount = 0
+                while c % divisor == 0:
+                    exponentCount += 1
+                    c //= divisor
+                if divisor % 4 == 3 and exponentCount % 2 != 0:
+                    return False
+            divisor += 1
+        return c % 4 != 3
