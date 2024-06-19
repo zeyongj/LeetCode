944d42419ff745626336239c27f372e012163631
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2024-06-19 12:07:37 -0700

    Time: 803 ms (65.83%), Space: 30.4 MB (75.28%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.py
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.py
@@ -1,36 +1,29 @@
-class Solution:
-    def get_num_of_bouquets(self, bloomDay, mid, k):
-        num_of_bouquets = 0
-        count = 0
-
-        for day in bloomDay:
-            # If the flower is bloomed, add to the set. Else reset the count.
-            if day <= mid:
-                count += 1
-            else:
-                count = 0
-
-            if count == k:
-                num_of_bouquets += 1
-                count = 0
-
-        return num_of_bouquets
-
+class Solution(object):
     def minDays(self, bloomDay, m, k):
         if m * k > len(bloomDay):
             return -1
-
-        start = 0
-        end = max(bloomDay)
-        minDays = -1
-
-        while start <= end:
-            mid = (start + end) // 2
-
-            if self.get_num_of_bouquets(bloomDay, mid, k) >= m:
-                minDays = mid
-                end = mid - 1
+        
+        def canMakeBouquets(bloomDay, m, k, day):
+            total = 0
+            flowers = 0
+            for b in bloomDay:
+                if b <= day:
+                    flowers += 1
+                    if flowers == k:
+                        total += 1
+                        flowers = 0
+                else:
+                    flowers = 0
+                if total >= m:
+                    return True
+            return False
+        
+        low, high = 1, max(bloomDay)
+        while low < high:
+            mid = (low + high) // 2
+            if canMakeBouquets(bloomDay, m, k, mid):
+                high = mid
             else:
-                start = mid + 1
-
-        return minDays
+                low = mid + 1
+        
+        return low
