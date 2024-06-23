fc6941f87384c90bbe7bcda441dda6a3947e4ba3
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2024-06-23 15:31:27 -0700

    Time: 448 ms (60.97%), Space: 26.4 MB (45.17%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -1,28 +1,26 @@
-class Solution:
+class Solution(object):
     def longestSubarray(self, nums, limit):
-        max_heap = []
-        min_heap = []
-
-        left = 0
+        increase = deque()
+        decrease = deque()
         max_length = 0
+        left = 0
 
         for right in range(len(nums)):
-            heapq.heappush(max_heap, (-nums[right], right))
-            heapq.heappush(min_heap, (nums[right], right))
-
-            # Check if the absolute difference between the maximum and minimum values in the current window exceeds the limit
-            while -max_heap[0][0] - min_heap[0][0] > limit:
-                # Move the left pointer to the right until the condition is satisfied.
-                # This ensures we remove the element causing the violation
-                left = min(max_heap[0][1], min_heap[0][1]) + 1
-
-                # Remove elements from the heaps that are outside the current window
-                while max_heap[0][1] < left:
-                    heapq.heappop(max_heap)
-                while min_heap[0][1] < left:
-                    heapq.heappop(min_heap)
-
-            # Update max_length with the length of the current valid window
+            while increase and nums[right] < increase[-1]:
+                increase.pop()
+            increase.append(nums[right])
+            
+            while decrease and nums[right] > decrease[-1]:
+                decrease.pop()
+            decrease.append(nums[right])
+            
+            while decrease[0] - increase[0] > limit:
+                if nums[left] == increase[0]:
+                    increase.popleft()
+                if nums[left] == decrease[0]:
+                    decrease.popleft()
+                left += 1
+                
             max_length = max(max_length, right - left + 1)
-
+        
         return max_length
