e8a358e4d7bfe54b958230bd282107260334d37f
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2024-06-30 11:04:34 -0700

    Time: 490 ms (100.00%), Space: 154.8 MB (100.00%) - LeetHub

--- 2745-construct-the-longest-new-string/2745-construct-the-longest-new-string.cpp
+++ 2745-construct-the-longest-new-string/2745-construct-the-longest-new-string.cpp
@@ -1,14 +1,14 @@
 class Solution {
-public:
-    int longestString(int x, int y, int z) {
-        int ans = 0;
-        if (x == y)
-            ans = 2 * min(x,y);
-        else
-            ans = 2 * min(x,y) + 1;
-            
-        ans += z;
-
-        return ans * 2;
+  int longestString(int x, int y, int z) {
+    int ans = 0;
+    if (x == y) {
+      ans = 2 * (x < y ? x : y);
+    } else {
+      ans = 2 * (x < y ? x : y) + 1;
     }
-};
+
+    ans += z;
+
+    return ans * 2;
+  }
+}
