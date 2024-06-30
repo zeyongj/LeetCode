9c28a04798a2870ff6d6e59557be3c0179d89d22
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2024-06-30 10:50:35 -0700

    Time: 1 ms (100.00%), Space: 41 MB (90.97%) - LeetHub

---
+++ 2745-construct-the-longest-new-string/2745-construct-the-longest-new-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int longestString(int x, int y, int z) {
+        int ans = 0;
+        if (x == y)
+            ans = 2 * Math.min(x,y);
+        else
+            ans = 2 * Math.min(x,y) + 1;
+            
+        ans += z;
+
+        return ans * 2;        
+    }
+}
