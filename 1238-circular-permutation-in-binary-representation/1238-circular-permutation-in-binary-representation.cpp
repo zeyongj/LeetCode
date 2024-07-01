ecb1e08a2f86ad9b2261905cbc85755a7018b1e1
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2024-07-01 13:09:37 -0700

    Time: 7 ms (83.87%), Space: 18.9 MB (56.63%) - LeetHub

---
+++ 1238-circular-permutation-in-binary-representation/1238-circular-permutation-in-binary-representation.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<int> circularPermutation(int n, int start) {
+        vector<int> res;
+        for (int i = 0; i < 1 << n; ++i)
+            res.push_back(start ^ i ^ i >> 1);
+        return res;
+    }
+};
