6842e8f231a13df03a5c1cb50ca95a01f70036dd
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2024-07-06 09:33:42 -0700

    Time: 40 ms (23.04%), Space: 16.4 MB (77.72%) - LeetHub

---
+++ 2582-pass-the-pillow/2582-pass-the-pillow.py
@@ -0,0 +1,13 @@
+class Solution:
+    def passThePillow(self, n: int, time: int) -> int:
+        current_pillow_position = 1
+        current_time = 0
+        direction = 1
+        while current_time < time:
+            if 0 < current_pillow_position + direction <= n:
+                current_pillow_position += direction
+                current_time += 1
+            else:
+                # Reverse the direction if the next position is out of bounds
+                direction *= -1
+        return current_pillow_position
