7e75f1a84c22b441293bc139f3f6787840cc8a27
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2024-07-11 12:29:26 -0700

    Time: 0 ms (100.00%), Space: 41 MB (6.53%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int trailingZeroes(int n) {
+        int cnt = 0;
+        while(n > 0){
+            n = n / 5;
+            cnt  = cnt + n;
+        }
+        return cnt;        
+    }
+}
