db569200f29ef81254dcc817f0641f940ae59342
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2024-08-31 08:06:43 -0700

    Time: 163 ms (91.97%), Space: 20.3 MB (59.43%) - LeetHub

---
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -0,0 +1,29 @@
+class UF:
+    def __init__(self, N):
+        self.parents = list(range(N))
+    def union(self, child, parent):
+        self.parents[self.find(child)] = self.find(parent)
+    def find(self, x):
+        if x != self.parents[x]:
+            self.parents[x] = self.find(self.parents[x])
+        return self.parents[x]
+    
+class Solution:
+    # 196 ms, 82.09%. 
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        uf = UF(len(accounts))
+        
+        # Creat unions between indexes
+        ownership = {}
+        for i, (_, *emails) in enumerate(accounts):
+            for email in emails:
+                if email in ownership:
+                    uf.union(i, ownership[email])
+                ownership[email] = i
+        
+        # Append emails to correct index
+        ans = collections.defaultdict(list)
+        for email, owner in ownership.items():
+            ans[uf.find(owner)].append(email)
+        
+        return [[accounts[i][0]] + sorted(emails) for i, emails in ans.items()]
