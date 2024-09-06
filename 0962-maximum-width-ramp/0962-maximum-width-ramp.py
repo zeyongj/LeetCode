8f9461d25b7a6c4c112e7649a02923960b22127f
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2024-09-06 12:18:26 -0700

    Time: 262 ms (30.43%), Space: 27.1 MB (10.22%) - LeetHub

--- 0962-maximum-width-ramp/0962-maximum-width-ramp.py
+++ 0962-maximum-width-ramp/0962-maximum-width-ramp.py
@@ -1,3 +1,6 @@
 class Solution:
     def maxWidthRamp(self, a: List[int]) -> int:
-        return max(map(sub,a:=[i for _,i in sorted((v,i) for i,v in enumerate(a))],accumulate(a,min)))
+        a = [i for _,i in sorted((v,i) for i,v in enumerate(a))]
+        b = accumulate(a, min)
+
+        return max(map(sub, a, b))
