c746a46d094c58803322c24e910cc1835e5e87ea
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2024-09-15 07:30:42 -0700

    Time: 147 ms (58.93%), Space: 17.4 MB (98.81%) - LeetHub

---
+++ 2207-maximize-number-of-subsequences-in-a-string/2207-maximize-number-of-subsequences-in-a-string.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maximumSubsequenceCount(self, text, pattern):
+        res = cnt1 = cnt2 = 0
+        for c in text:
+            if c == pattern[1]:
+                res += cnt1
+                cnt2 += 1
+            if c == pattern[0]:
+                cnt1 += 1
+        return res + max(cnt1, cnt2)
