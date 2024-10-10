33e19008295e513ab448cd06b6da6662ebb74d65
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2024-10-10 11:49:38 -0700

    Time: 384 ms (23.86%), Space: 23.7 MB (56.38%) - LeetHub

--- 0962-maximum-width-ramp/0962-maximum-width-ramp.py
+++ 0962-maximum-width-ramp/0962-maximum-width-ramp.py
@@ -1,6 +1,22 @@
 class Solution:
-    def maxWidthRamp(self, a: List[int]) -> int:
-        a = [i for _,i in sorted((v,i) for i,v in enumerate(a))]
-        b = accumulate(a, min)
+    def maxWidthRamp(self, nums):
+        n = len(nums)
+        rightMax = [0] * n
+        rightMax[n - 1] = nums[n - 1]
 
-        return max(map(sub, a, b))
+        # Fill the rightMax array
+        for i in range(n - 2, -1, -1):
+            rightMax[i] = max(rightMax[i + 1], nums[i])
+
+        left, right, maxVal = 0, 0, 0
+
+        # Find the maximum width ramp
+        while right < n:
+            while right < n and nums[left] <= rightMax[right]:
+                right += 1
+            
+            maxVal = max(maxVal, right - left - 1)
+            left += 1
+            right = left + maxVal + 1
+
+        return maxVal
