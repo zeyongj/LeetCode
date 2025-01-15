39fbbae2a5b661540e9ffa62e53dcfd725cba81b
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2025-01-15 10:05:51 -0800

    Time: 0 ms (100%), Space: 17.7 MB (59.2%) - LeetHub

---
+++ 2509-minimize-xor/2509-minimize-xor.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def minimizeXor(self, num1, num2):
+        a, b = bin(num1).count('1'), bin(num2).count('1')
+        res = num1
+        for i in range(32):
+            if a > b and (1 << i) & num1 > 0:
+                res ^= 1 << i
+                a -= 1
+            if a < b and (1 << i) & num1 == 0:
+                res ^= 1 << i
+                a += 1
+        return res
