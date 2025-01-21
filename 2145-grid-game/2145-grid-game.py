092b128d1ed891ea0467f616e7b48dcfbf7a5915
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2025-01-21 01:16:19 -0800

    Time: 77 ms (89.84%), Space: 29.7 MB (62.95%) - LeetHub

---
+++ 2145-grid-game/2145-grid-game.py
@@ -0,0 +1,12 @@
+class Solution:
+    def gridGame(self, grid: List[List[int]]) -> int:
+        min_result = float('inf')
+        row1_sum = sum(grid[0])
+        row2_sum = 0
+        
+        for i in range(len(grid[0])):
+            row1_sum -= grid[0][i]
+            min_result = min(min_result, max(row1_sum, row2_sum))
+            row2_sum += grid[1][i]
+        
+        return min_result
