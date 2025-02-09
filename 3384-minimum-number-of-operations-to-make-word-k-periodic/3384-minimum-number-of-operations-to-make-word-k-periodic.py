ea3223334dc62ea2583a893e75c8b904a40f529b
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2025-02-08 23:54:51 -0800

    Time: 59 ms (90.57%), Space: 19.8 MB (20.13%) - LeetHub

---
+++ 3384-minimum-number-of-operations-to-make-word-k-periodic/3384-minimum-number-of-operations-to-make-word-k-periodic.py
@@ -0,0 +1,3 @@
+class Solution:
+    def minimumOperationsToMakeKPeriodic(self, s: str, k: int) -> int:
+        return (len(s) // k) - max(Counter(s[i : i + k] for i in range(0, len(s), k)).values())
