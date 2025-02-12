e3168b30b2b35ae36211fa68e7b2582d4c9bb0bb
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2025-02-11 21:34:14 -0800

    Time: 574 ms (32.64%), Space: 31.4 MB (82.84%) - LeetHub

---
+++ 0220-contains-duplicate-iii/0220-contains-duplicate-iii.py
@@ -0,0 +1,15 @@
+from sortedcontainers import SortedList
+
+class Solution:
+    def containsNearbyAlmostDuplicate(self, nums, k, t):
+        SList = SortedList()
+        for i in range(len(nums)):
+            if i > k: SList.remove(nums[i-k-1])   
+            pos1 = SortedList.bisect_left(SList, nums[i] - t)
+            pos2 = SortedList.bisect_right(SList, nums[i] + t)
+            
+            if pos1 != pos2 and pos1 != len(SList): return True
+            
+            SList.add(nums[i])
+        
+        return False
