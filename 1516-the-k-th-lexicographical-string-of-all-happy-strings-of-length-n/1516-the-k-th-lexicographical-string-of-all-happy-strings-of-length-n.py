698178d6517600282cbf4360327580f45a5321b4
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2025-02-18 23:46:34 -0800

    Time: 0 ms (100%), Space: 17.7 MB (94.09%) - LeetHub

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
@@ -1,14 +1,17 @@
 class Solution:
     def getHappyString(self, n: int, k: int) -> str:
-        def generate(prev):
-            if len(prev) == n:
-                yield prev
-                return
+        def dfs(prefix, n, k):
+            if n == 0:
+                return prefix
             for c in 'abc':
-                if not prev or c != prev[-1]:
-                    yield from generate(prev + c)
-            
-        for i, res in enumerate(generate(''), 1):            
-            if i == k:
-                return res
-        return ''  
+                if prefix and c == prefix[-1]:
+                    continue
+                cnt = 2 ** (n2 - len(prefix) - 1)
+                if cnt >= k:
+                    return dfs(prefix + c, n - 1, k)
+                else:
+                    k -= cnt
+            return ""
+        
+        n2 = n
+        return dfs("", n, k)
