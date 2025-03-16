13ad7881e12ab79e5422ad5e2fa97ca979ecad61
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2025-03-15 19:33:27 -0700

    Time: 12 ms (73%), Space: 20 MB (73.32%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,26 +1,18 @@
 class Solution:
-    def minPathSum(self, grid):
-        m = len(grid)
-        n = len(grid[0])
-
-        # Initialize the dynamic programming table with the size of grid
-        dp = [[0]*n for _ in range(m)]
-
-        # Initialize the top left cell
-        dp[0][0] = grid[0][0]
-
-        # Calculate the minimum path sum for the first row
-        for j in range(1, n):
-            dp[0][j] = dp[0][j-1] + grid[0][j]
-
-        # Calculate the minimum path sum for the first column
+    def minPathSum(self, grid: List[List[int]]) -> int:
+            
+        m, n = len(grid), len(grid[0])
+        
         for i in range(1, m):
-            dp[i][0] = dp[i-1][0] + grid[i][0]
-
-        # Calculate the minimum path sum for each remaining cell
+            grid[i][0] += grid[i-1][0]
+        
+        for i in range(1, n):
+            grid[0][i] += grid[0][i-1]
+        
         for i in range(1, m):
             for j in range(1, n):
-                dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
-
-        # Return the minimum path sum to reach the bottom right cell
-        return dp[-1][-1]
+                grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+        
+        return grid[-1][-1]
+    
+        # An Upvote will be encouraging
