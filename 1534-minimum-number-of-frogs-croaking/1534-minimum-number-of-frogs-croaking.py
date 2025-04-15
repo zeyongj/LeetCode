6e7ba8a2c4146823ebb3b7710dd6e805da73271d
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2025-04-15 12:29:34 -0700

    Time: 77 ms (79.02%), Space: 18.1 MB (35.97%) - LeetHub

---
+++ 1534-minimum-number-of-frogs-croaking/1534-minimum-number-of-frogs-croaking.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minNumberOfFrogs(self, croak):
+        c, r, o, a, k, in_use, answer = 0, 0, 0, 0, 0, 0, 0
+        for d in croak:
+            if d == 'c':
+                c, in_use = c+1, in_use+1
+            elif d == 'r':
+                r += 1
+            elif d == 'o':
+                o += 1
+            elif d == 'a':
+                a += 1
+            else:
+                k, in_use = k+1, in_use-1
+                
+            answer = max(answer, in_use)
+            
+            if c < r or r < o or o < a or a < k:
+                return -1
+            
+        if in_use == 0 and c == r and r == o and o == a and a == k:
+            return answer
+        
+        return -1
