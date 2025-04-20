2c8d012c46fa1b112b177f93597a3ba2b4da68fe
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2025-04-20 16:13:37 -0700

    Time: 0 ms (100%), Space: 17.9 MB (76.23%) - LeetHub

--- 0797-rabbits-in-forest/0797-rabbits-in-forest.py
+++ 0797-rabbits-in-forest/0797-rabbits-in-forest.py
@@ -1,21 +1,7 @@
-class Solution:
-    def numRabbits(self, answers: List[int]) -> int:
-        d = {}
-        count = 0
-        for i in answers:
-		# add 1 if rabbit says 0 other rabbits have same color
-            if i == 0:
-                count+=1
-            else:
-			# check if i is present in dictionary or not 
-			# and also check whether the frequency of i and value of i is same or not
-			# For example [2,2,2] and [2,2] has the same result (i.e) 3 but [2,2,2,2] should            
-			# be seperated into two groups and the result will be 6
-            # So we are again initializing the i value to 0
-                if i not in d or i == d[i]:
-                    d[i] = 0
-                    count+=1 + i
-                else:
-                    d[i] += 1
-                    
-        return count
+class Solution(object):
+    def numRabbits(self, answers):
+        mpp = Counter(answers)
+        total = 0
+        for x in mpp:
+            total += ceil(float(mpp[x]) / (x + 1)) * (x + 1)
+        return int(total)
