8cffdd84c1271281458010bf335dfa9777596cc4
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2025-04-21 12:08:33 -0700

    Time: 1 ms (82%), Space: 21.9 MB (27.39%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
@@ -1,10 +1,13 @@
-class NumArray:
-    def __init__(self, nums):
-        self.arr = [0] + list(accumulate(nums))
-        
-    def sumRange(self, left, right):
-        return self.arr[right + 1] - self.arr[left]
+class NumArray:  # 68 ms, faster than 97.72%
 
+    def __init__(self, nums: List[int]):
+        self.preSum = nums  # pass by pointer!
+        for i in range(len(nums)-1):
+            self.preSum[i+1] += self.preSum[i]
+
+    def sumRange(self, left: int, right: int) -> int:
+        if left == 0: return self.preSum[right]
+        return self.preSum[right] - self.preSum[left-1]
 
 # Your NumArray object will be instantiated and called as such:
 # obj = NumArray(nums)
