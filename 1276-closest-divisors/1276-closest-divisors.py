0a42b705a23f91ae1bb1dc555a2152e62240af00
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2025-05-06 11:17:10 -0700

    Time: 67 ms (89.91%), Space: 17.9 MB (27.52%) - LeetHub

---
+++ 1276-closest-divisors/1276-closest-divisors.py
@@ -0,0 +1,8 @@
+class Solution:
+	def closestDivisors(self, num: int) -> List[int]:
+		for i in range(round((num+2)**0.5),0,-1):
+			if (num+1)%i==0:
+				return [i,(num+1)//i]
+			if (num+2)%i==0:
+				return [i,(num+2)//i]
+				
