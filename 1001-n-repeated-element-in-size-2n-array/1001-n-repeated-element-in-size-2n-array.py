9aef97f8638801a17d7e74c85d1fc0c3014c9ef6
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2025-05-16 13:18:16 -0700

    Time: 0 ms (100%), Space: 18.9 MB (30.01%) - LeetHub

--- 1001-n-repeated-element-in-size-2n-array/1001-n-repeated-element-in-size-2n-array.py
+++ 1001-n-repeated-element-in-size-2n-array/1001-n-repeated-element-in-size-2n-array.py
@@ -1,13 +1,9 @@
-class Solution(object):
-    def repeatedNTimes(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+class Solution:
+    def repeatedNTimes(self, nums: List[int]) -> int:
         new_set = set()
         for i in nums :
             if i not in new_set :
                 new_set.add(i)
             else :
                 return i
-        return nums[0]
+        return nums[0]        
