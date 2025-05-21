a40f9ec03362532a3e0a131ca59f249ff7401d28
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2025-05-21 13:46:58 -0700

    Time: 0 ms (100%), Space: 18.6 MB (37.5%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,34 +1,17 @@
-class Solution:
-    def setZeroes(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
-        if not matrix:
-            return
-        
-        m, n = len(matrix), len(matrix[0])
-        first_row_has_zero = any(matrix[0][j] == 0 for j in range(n))
-        first_col_has_zero = any(matrix[i][0] == 0 for i in range(m))
-        
-        # Use the first row and first column to mark zeros
-        for i in range(1, m):
-            for j in range(1, n):
-                if matrix[i][j] == 0:
-                    matrix[i][0] = 0
-                    matrix[0][j] = 0
-        
-        # Zero out cells based on marks in the first row and column
-        for i in range(1, m):
-            for j in range(1, n):
-                if matrix[i][0] == 0 or matrix[0][j] == 0:
-                    matrix[i][j] = 0
-        
-        # Zero out the first row if needed
-        if first_row_has_zero:
-            for j in range(n):
-                matrix[0][j] = 0
-        
-        # Zero out the first column if needed
-        if first_col_has_zero:
-            for i in range(m):
-                matrix[i][0] = 0
+class Solution(object):
+    def setZeroes(self, matrix):
+        zeroinFirstCol = False
+        for row in range(len(matrix)):
+            if matrix[row][0] == 0:
+                zeroinFirstCol = True
+            for col in range(1, len(matrix[0])):
+                if matrix[row][col] == 0:
+                    matrix[row][0] = 0
+                    matrix[0][col] = 0
+
+        for row in range(len(matrix) - 1, -1, -1):
+            for col in range(len(matrix[0]) - 1, 0, -1):
+                if matrix[row][0] == 0 or matrix[0][col] == 0:
+                    matrix[row][col] = 0
+            if zeroinFirstCol:
+                matrix[row][0] = 0
