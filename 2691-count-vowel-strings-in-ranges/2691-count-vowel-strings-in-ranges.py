c1f9a356f280188d2b846436ee12baba3f16064c
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2025-05-28 12:22:28 -0700

    Time: 27 ms (19.4%), Space: 50.3 MB (19.91%) - LeetHub

--- 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.py
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.py
@@ -1,22 +1,13 @@
 class Solution:
-    def vowelStrings(self, words, queries):
-        prefix = []
-        res = []
-        count = 0
-
-        def isVowel(c):
-            return c in 'aeiou'
-
-        for word in words:
-            if isVowel(word[0]) and isVowel(word[-1]):
-                count += 1
-            prefix.append(count)
-
-        for query in queries:
-            l, r = query
-            if l == 0:
-                res.append(prefix[r])
-            else:
-                res.append(prefix[r] - prefix[l - 1])
-
-        return res
+    def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
+        isVowel=1+(1<<(ord('e')-ord('a')))+(1<<(ord('i')-ord('a')))+(1<<(ord('o')-ord('a')))+(1<<(ord('u')-ord('a')))
+        n, qz=len(words), len(queries)
+        cnt=[0]*(n+1)
+        for i, w in enumerate(words):
+            cnt[i+1]=cnt[i]+(((1<<(ord(w[0])-ord('a')))& isVowel)!=0 and ((1<<(ord(w[-1])-ord('a')))& isVowel)!=0)
+        ans=[0]*qz
+        for i, (q0, q1) in enumerate(queries):
+            ans[i]=cnt[q1+1]-cnt[q0]
+        return ans
+        
+        
