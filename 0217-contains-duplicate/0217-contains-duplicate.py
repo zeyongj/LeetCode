56020c42dfb8f5fc3698b61530a4f63ee5a0ae19
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2025-06-25 12:15:17 -0700

    Time: 44 ms (11.01%), Space: 35 MB (9.96%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,49 +1,10 @@
 class Solution:
+    from collections import Counter
     def containsDuplicate(self, nums: List[int]) -> bool:
-        size = len(nums)
-        
-        if size == 0:
-            return False
-        
-        numDict = {}
-        
-        for num in nums:
-            if num not in numDict.keys():
-                numDict[num] = 1
-            else:
-                val = numDict.get(num)
-                val += 1
-                numDict.update({num:val})
-        
-        occurances = numDict.values()
-        
-        for occurance in occurances:
-            if occurance != 1:
+        a=Counter(nums)
+        for i in a:
+            if a[i]>1:
                 return True
-        
         return False
+                
         
-#         for num in nums:
-#             if num not in numDict.keys():
-#                 numDict[num] = 1
-#             else:
-#                 return True
-            
-#         return False
-
-
-        
-#         mySet = set()
-        
-#         for num in nums:
-#             mySet.add(num)
-        
-#         setLength = len(mySet)
-        
-#         if size == setLength:
-#             return False
-#         else:
-#             return True
-        
-    
-    
