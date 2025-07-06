06d2ddd57c7641c4079c30ba178630cd3638f2ca
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2025-07-06 14:51:15 -0700

    Time: 0 ms (100%), Space: 17.7 MB (64.3%) - LeetHub

---
+++ 1781-check-if-two-string-arrays-are-equivalent/1781-check-if-two-string-arrays-are-equivalent.py
@@ -0,0 +1,12 @@
+class Solution:
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        for c1, c2 in zip(self.generate(word1), self.generate(word2)):
+            if c1 != c2:
+                return False
+        return True
+
+    def generate(self, wordlist: List[str]):
+        for word in wordlist:
+            for char in word:
+                yield char
+        yield None
