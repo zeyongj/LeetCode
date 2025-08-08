6a321ff201fff7394fff39e2cab66179446eba8e
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2025-08-08 12:18:05 -0700

    Time: 5 ms (97.36%), Space: 19.5 MB (56.11%) - LeetHub

--- 0826-soup-servings/0826-soup-servings.py
+++ 0826-soup-servings/0826-soup-servings.py
@@ -1,12 +1,25 @@
+from functools import lru_cache
+import math
+
 class Solution:
     def soupServings(self, n: int) -> float:
-        if n>50000: return 1.0
-        @cache
-        def dfs(A, B):
-            if A<=0 and B>0: return 1
-            if A<=0 and B<=0: return 0.5
-            if A>0 and B<=0: return 0
-            return 0.25*(dfs(A-4, B)+dfs(A-3, B-1)+dfs(A-2, B-2)+dfs(A-1, B-3))
-        N=(n+24)//25
-        return dfs(N, N)
-        
+        if n > 5000:
+            return 1.0
+        units = math.ceil(n / 25)
+
+        @lru_cache(None)
+        def calc_prob(soupA, soupB):
+            if soupA <= 0 and soupB <= 0:
+                return 0.5
+            if soupA <= 0:
+                return 1.0
+            if soupB <= 0:
+                return 0.0
+            return 0.25 * (
+                calc_prob(soupA - 4, soupB) +
+                calc_prob(soupA - 3, soupB - 1) +
+                calc_prob(soupA - 2, soupB - 2) +
+                calc_prob(soupA - 1, soupB - 3)
+            )
+
+        return calc_prob(units, units)
