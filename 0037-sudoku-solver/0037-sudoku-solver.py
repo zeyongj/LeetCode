7ad05bf2aa2d8c2a10570c8e832c8842b0ab76e5
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2025-08-30 20:12:52 -0700

    Time: 3371 ms (5.14%), Space: 18.1 MB (21.32%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.py
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -1,36 +1,52 @@
 class Solution:
-    def solveSudoku(self, board: List[List[str]]) -> None:
-        def is_valid(board, row, col, num):
-            for x in range(9):
-                if board[row][x] == num:
-                    return False
-                    
-            for x in range(9):
-                if board[x][col] == num:
-                    return False
-            
-            start_row, start_col = row - row%3, col - col%3
-            for i in range(3):
-                for j in range(3):
-                    if board[i+start_row][j+start_col] == num:
-                        return False
-            return True
+    def solveSudoku(self, board: list[list[str]]) -> None:
+        n, N = 3, 9
+        rows = [[0] * (N + 1) for _ in range(N)]
+        cols = [[0] * (N + 1) for _ in range(N)]
+        boxes = [[0] * (N + 1) for _ in range(N)]
+        sudokuSolved = False
 
-        def solve(board):
-            for i in range(9):
-                for j in range(9):
-                    if board[i][j] == '.':
-                        for num in "123456789":
-                            if is_valid(board, i, j, num):
-                                board[i][j] = num
+        def couldPlace(d, row, col):
+            idx = (row // n) * n + col // n
+            return rows[row][d] + cols[col][d] + boxes[idx][d] == 0
 
-                                if solve(board):
-                                    return True
-                                else:
-                                    board[i][j] = '.'  # undo if no valid number can be placed
-                        return False  # trigger backtracking
-            return True  # all cells are filled
-            
-        solve(board)
+        def placeNumber(d, row, col):
+            idx = (row // n) * n + col // n
+            rows[row][d] += 1
+            cols[col][d] += 1
+            boxes[idx][d] += 1
+            board[row][col] = str(d)
 
-        
+        def removeNumber(d, row, col):
+            idx = (row // n) * n + col // n
+            rows[row][d] -= 1
+            cols[col][d] -= 1
+            boxes[idx][d] -= 1
+            board[row][col] = '.'
+
+        def placeNextNumbers(row, col):
+            nonlocal sudokuSolved
+            if row == N - 1 and col == N - 1:
+                sudokuSolved = True
+            elif col == N - 1:
+                backtrack(row + 1, 0)
+            else:
+                backtrack(row, col + 1)
+
+        def backtrack(row, col):
+            nonlocal sudokuSolved
+            if board[row][col] == '.':
+                for d in range(1, 10):
+                    if couldPlace(d, row, col):
+                        placeNumber(d, row, col)
+                        placeNextNumbers(row, col)
+                        if not sudokuSolved:
+                            removeNumber(d, row, col)
+            else:
+                placeNextNumbers(row, col)
+
+        for i in range(N):
+            for j in range(N):
+                if board[i][j] != '.':
+                    placeNumber(int(board[i][j]), i, j)
+        backtrack(0, 0)
