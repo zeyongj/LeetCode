902aac2820d1944544be5081f0b68f53dc57a596
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2025-09-02 12:28:29 -0700

    Time: 19 ms (74.36%), Space: 17.7 MB (87.18%) - LeetHub

--- 3278-find-the-number-of-ways-to-place-people-i/3278-find-the-number-of-ways-to-place-people-i.py
+++ 3278-find-the-number-of-ways-to-place-people-i/3278-find-the-number-of-ways-to-place-people-i.py
@@ -1,30 +1,11 @@
 class Solution:
-    def numberOfPairs(self, v):
-        n = len(v)
-        ans = 0
-
-        for i in range(n):
-            x1, y1 = v[i][0], v[i][1]
-
-            for j in range(n):
-                if i == j:
-                    continue
-
-                x2, y2 = v[j][0], v[j][1]
-                f = 0
-
-                if y1 >= y2 and x1 <= x2:
-                    for k in range(n):
-                        if k == i or k == j:
-                            continue
-
-                        x3, y3 = v[k][0], v[k][1]
-
-                        if x3 >= x1 and x3 <= x2 and y3 >= y2 and y3 <= y1:
-                            f = 1
-                            break
-
-                    if f == 0:
-                        ans += 1
-
+    def numberOfPairs(self, P: List[List[int]]) -> int:
+        P.sort(key=lambda p: (-p[0], p[1]))
+        n, ans=len(P), 0
+        for i in range(n-1):
+            y=1<<31
+            for j in range(i+1, n):
+                if y>P[j][1]>=P[i][1]:
+                    ans+=1
+                    y=P[j][1]
         return ans
