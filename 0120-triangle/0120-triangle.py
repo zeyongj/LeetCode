bf0dfef516a49886fe3552b7f75faa57dedb5b37
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2025-09-25 10:50:49 -0700

    Time: 7 ms (33.7%), Space: 19.2 MB (46.7%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,10 +1,6 @@
 class Solution:
-    def minimumTotal(self, triangle: List[List[int]]) -> int:
-        size = len(triangle) 
-        
-        
-        for row in range(size - 2, -1, -1):
-            for col in range(len(triangle[row])):
-                triangle[row][col] += min(triangle[row+1][col], triangle[row+1][col+1]) # Adjacent Num
-                
-        return triangle[0][0]
+    def minimumTotal(self, tri: List[List[int]]) -> int:
+        for i in range(len(tri) - 2, -1, -1):
+            for j in range(len(tri[i])):
+                tri[i][j] += min(tri[i + 1][j], tri[i + 1][j + 1])
+        return tri[0][0]
