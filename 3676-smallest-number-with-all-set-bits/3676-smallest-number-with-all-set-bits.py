0f21cdb5c3da438ed995b2d012e6661e6d2395c3
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2025-10-29 00:20:57 -0700

    Time: 1 ms (20.33%), Space: 17.7 MB (75.09%) - LeetHub

--- 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.py
+++ 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.py
@@ -1,3 +1,7 @@
-class Solution:
-    def smallestNumber(self, n: int) -> int:
-        return (1 << n.bit_length()) - 1   
+class Solution(object):
+    def smallestNumber(self, n):
+        for i in range(n + 1):
+            z = 1 << i
+            if z > n:
+                return z - 1
+        return 0
