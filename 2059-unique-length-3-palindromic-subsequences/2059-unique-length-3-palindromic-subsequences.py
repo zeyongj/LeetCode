f8c4ca9d3f9d62ee1e35700f97ad4a420540d875
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2025-11-21 00:25:36 -0800

    Time: 895 ms (31.2%), Space: 18.5 MB (26.6%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.py
@@ -1,18 +1,28 @@
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
-        R = [0] * 26
-        for u in s:
-            R[ord(u) - ord('a')] += 1
-        
-        L = [0] * 26
-        S = set()
-        
-        for i in range(len(s)):
-            t = ord(s[i]) - ord('a')
-            R[t] -= 1
-            for j in range(26):
-                if L[j] > 0 and R[j] > 0:
-                    S.add(26 * t + j)
-            L[t] += 1
-        
-        return len(S)
+        n = len(s)
+        first = [n] * 26
+        last = [-1] * 26
+
+        # find first & last
+        for i, ch in enumerate(s):
+            idx = ord(ch) - ord('a')
+            first[idx] = min(first[idx], i)
+            last[idx] = i
+
+        res = 0
+
+        for c in range(26):
+            L, R = first[c], last[c]
+            if R - L < 2:
+                continue
+
+            seen = [False] * 26
+
+            for i in range(L + 1, R):
+                idx = ord(s[i]) - ord('a')
+                if not seen[idx]:
+                    seen[idx] = True
+                    res += 1
+
+        return res
