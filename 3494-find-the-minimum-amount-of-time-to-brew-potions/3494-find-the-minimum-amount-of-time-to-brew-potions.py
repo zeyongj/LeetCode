465c7c1b0c3a68106e0103a356e283088d627dd8
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2025-11-23 01:30:29 -0800

    Time: 6765 ms (77.74%), Space: 19.3 MB (5.78%) - LeetHub

--- 3494-find-the-minimum-amount-of-time-to-brew-potions/3494-find-the-minimum-amount-of-time-to-brew-potions.py
+++ 3494-find-the-minimum-amount-of-time-to-brew-potions/3494-find-the-minimum-amount-of-time-to-brew-potions.py
@@ -1,13 +1,9 @@
 class Solution:
-    def minTime(self, skill: list[int], mana: list[int]) -> int:
-        n = len(skill)
-        time = [0] * n
+    def minTime(self, a: List[int], b: List[int]) -> int:
+        q = [0]*(len(a)+1)
+        for p in b:
+            qq = [0,*accumulate(v*p for v in a)]
+            dt = max(map(sub,q[1:],qq))
+            q = [t+dt for t in qq]
 
-        for x in mana:
-            time[0] = time[0] + skill[0] * x
-            for i in range(1, n):
-                time[i] = max(time[i], time[i - 1]) + skill[i] * x
-            for i in range(n - 2, -1, -1):
-                time[i] = time[i + 1] - skill[i + 1] * x
-
-        return time[-1]
+        return q[-1]
