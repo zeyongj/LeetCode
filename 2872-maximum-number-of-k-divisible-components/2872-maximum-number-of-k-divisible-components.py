5e7f56fbf44887ab153a1ed0bc45c72eb82d6b5b
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2025-11-28 01:23:26 -0800

    Time: 176 ms (18.12%), Space: 37.6 MB (75.36%) - LeetHub

--- 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.py
+++ 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.py
@@ -1,20 +1,36 @@
-from collections import defaultdict
 class Solution:
-    def maxKDivisibleComponents(self, n: int, edges: List[List[int]], values: List[int], k: int) -> int:
-        G = defaultdict(list)
-        for u,v in edges:
-            G[u].append(v)
-            G[v].append(u)
-        components = 1
-        def dfs(v, u):
-            nonlocal components
-            res = values[v]
-            for w in G[v]:
-                if w != u:
-                    cur = dfs(w, v)
-                    res += cur
-                    if cur%k == 0:
-                        components += 1
-            return res
-        dfs(0, -1)
-        return components
+    def __init__(self):
+        self.cnt = 0
+
+    def getSubtree(self, node, adj, parent, subtree):
+        if len(adj[node]) == 1 and adj[node][0] == parent:
+            return
+        for neighbor in adj[node]:
+            if neighbor != parent:
+                self.getSubtree(neighbor, adj, node, subtree)
+                subtree[node] += subtree[neighbor]
+
+    def dfs(self, node, adj, parent, subtree, k):
+        if len(adj[node]) == 1 and adj[node][0] == parent:
+            return
+        for neighbor in adj[node]:
+            if neighbor != parent:
+                parSubtree = subtree[node] - subtree[neighbor]
+                childSubtree = subtree[neighbor]
+                if parSubtree % k == 0 and childSubtree % k == 0:
+                    self.cnt += 1
+                    subtree[node] -= subtree[neighbor]
+                else:
+                    subtree[neighbor] = subtree[node]
+                self.dfs(neighbor, adj, node, subtree, k)
+
+    def maxKDivisibleComponents(self, n, edges, values, k):
+        adj = [[] for _ in range(n + 1)]
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+
+        subtree = [val for val in values]
+        self.getSubtree(0, adj, -1, subtree)
+        self.dfs(0, adj, -1, subtree, k)
+        return self.cnt + 1
