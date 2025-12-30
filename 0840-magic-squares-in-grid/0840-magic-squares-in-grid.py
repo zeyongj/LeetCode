255624420af7479a7e48ec403b70fb5b1f2b4a71
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2025-12-30 00:55:41 -0800

    Time: 0 ms (100%), Space: 17.1 MB (100%) - LeetHub

--- 0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
@@ -1,23 +1,36 @@
 class Solution:
-    def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
-        n = len(grid)
-        m = len(grid[0])
-        if n < 3 or m < 3:
-            return 0
-        cnt = 0
-        magic_sq = [
-            [[4, 9, 2], [3, 5, 7], [8, 1, 6]],
-            [[2, 7, 6], [9, 5, 1], [4, 3, 8]],
-            [[6, 1, 8], [7, 5, 3], [2, 9, 4]],
-            [[8, 3, 4], [1, 5, 9], [6, 7, 2]],
-            [[4, 3, 8], [9, 5, 1], [2, 7, 6]],
-            [[2, 9, 4], [7, 5, 3], [6, 1, 8]],
-            [[6, 7, 2], [1, 5, 9], [8, 3, 4]],
-            [[8, 1, 6], [3, 5, 7], [4, 9, 2]]
-        ]
-        for r_start in range(n - 2):
-            for c_start in range(m - 2):
-                subgrid = [grid[r_start + i][c_start:c_start + 3] for i in range(3)]
-                if subgrid in magic_sq:
-                    cnt += 1
-        return cnt
+    def numMagicSquaresInside(self, grid):
+        rows, cols = len(grid), len(grid[0])
+        count = 0
+
+        for i in range(rows - 2):
+            for j in range(cols - 2):
+                if self.isMagic(grid, i, j):
+                    count += 1
+        return count
+
+    def isMagic(self, g, r, c):
+        if g[r + 1][c + 1] != 5:
+            return False
+
+        seen = set()
+
+        for i in range(r, r + 3):
+            for j in range(c, c + 3):
+                val = g[i][j]
+                if val < 1 or val > 9 or val in seen:
+                    return False
+                seen.add(val)
+
+        for i in range(3):
+            if sum(g[r + i][c:c + 3]) != 15:
+                return False
+            if g[r][c + i] + g[r + 1][c + i] + g[r + 2][c + i] != 15:
+                return False
+
+        if g[r][c] + g[r + 1][c + 1] + g[r + 2][c + 2] != 15:
+            return False
+        if g[r][c + 2] + g[r + 1][c + 1] + g[r + 2][c] != 15:
+            return False
+
+        return True
