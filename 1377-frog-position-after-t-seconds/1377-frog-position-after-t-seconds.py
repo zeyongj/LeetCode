c1fd5790baa769f74fb5996c88e18eb751805917
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2026-01-03 14:20:25 -0800

    Time: 43 ms (5.08%), Space: 18 MB (55.73%) - LeetHub

---
+++ 1377-frog-position-after-t-seconds/1377-frog-position-after-t-seconds.py
@@ -0,0 +1,19 @@
+class Solution:
+    def frogPosition(self, n: int, edges: List[List[int]], t: int, target: int) -> float:
+        nei = collections.defaultdict(set)
+        for a, b in edges:
+            nei[a].add(b)
+            nei[b].add(a)
+            
+        visited, res = set(), 0.
+        def dfs(leaf_id, p, time):
+            nonlocal res
+            if time >= t:
+                if leaf_id == target: res = p
+                return
+            visited.add(leaf_id)
+            neighbors = nei[leaf_id] - visited
+            for n in neighbors or [leaf_id]:
+                dfs(n, p / (len(neighbors) or 1), time + 1)
+        dfs(1, 1, 0)
+        return res
