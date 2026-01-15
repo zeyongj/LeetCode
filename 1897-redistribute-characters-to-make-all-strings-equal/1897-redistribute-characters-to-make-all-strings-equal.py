ffc9aafd8c4eb57d8e81746bbbc61255ceeaf74a
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2026-01-15 01:01:46 -0800

    Time: 7 ms (93.13%), Space: 19.5 MB (6.87%) - LeetHub

--- 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.py
+++ 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.py
@@ -1,13 +1,21 @@
 class Solution:
     def makeEqual(self, words: List[str]) -> bool:
-        counts = defaultdict(int)
-        for word in words:
-            for c in word:
-                counts[c] += 1
-        
-        n = len(words)
-        for val in counts.values():
-            if val % n != 0:
+        if len(words) == 1:
+            return True
+
+        total_char_count = sum(len(s) for s in words)
+
+        if total_char_count % len(words) != 0:
+            return False
+
+        my_map = [0] * 26
+        for s in words:
+            for c in s:
+                my_map[ord(c) - ord('a')] += 1
+
+        for i in my_map:
+            if i % len(words) != 0:
                 return False
-        
+
         return True
+                
