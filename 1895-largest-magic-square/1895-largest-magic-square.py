b4dae1ed761b6ec9e90883c25b7e15a063aeaaef
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2026-01-18 01:23:31 -0800

    Time: 84 ms (75.86%), Space: 19.8 MB (12.07%) - LeetHub

--- 1895-largest-magic-square/1895-largest-magic-square.py
+++ 1895-largest-magic-square/1895-largest-magic-square.py
@@ -1,30 +1,49 @@
 class Solution:
     def largestMagicSquare(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
-        res = 1
+        n, m = len(grid), len(grid[0])
+        if n < 2 or m < 2:
+            return 1
 
-        def isValid(i, j, k):
-            s = None
-            for x in range(i, i + k):
-                row = sum(grid[x][j:j + k])
-                if s is None: s = row
-                elif s != row: return False
+        row_prefix = [[0]*(m+1) for _ in range(n)]
+        col_prefix = [[0]*(m) for _ in range(n+1)]
 
-            for y in range(j, j + k):
-                if sum(grid[x][y] for x in range(i, i + k)) != s:
-                    return False
+        for i in range(n):
+            for j in range(m):
+                row_prefix[i][j+1] = row_prefix[i][j] + grid[i][j]
+        
+        for j in range(m):
+            for i in range(n):
+                col_prefix[i+1][j] = col_prefix[i][j] + grid[i][j]
+        
+        for k in range(min(n, m), 1, -1):
+            for r in range(n - k + 1):
+                for c in range(m - k + 1):
+                        
+                    target = row_prefix[r][c+k] - row_prefix[r][c]
+                        
+                    magic = True
+                        
+                    for i in range(k):
+                        row_sum = row_prefix[r+i][c+k] - row_prefix[r+i][c]
+                        if row_sum != target:
+                            magic = False
+                            break
+                    if not magic: 
+                        continue
 
-            if sum(grid[i + d][j + d] for d in range(k)) != s:
-                return False
-
-            if sum(grid[i + d][j + k - 1 - d] for d in range(k)) != s:
-                return False
-
-            return True
-
-        for k in range(2, min(m, n) + 1):
-            for i in range(m - k + 1):
-                for j in range(n - k + 1):
-                    if isValid(i, j, k):
-                        res = k
-        return res
+                    for j in range(k):
+                        col_sum = col_prefix[r+k][c+j] - col_prefix[r][c+j]
+                        if col_sum != target:
+                            magic = False
+                            break
+                    if not magic: 
+                        continue
+                            
+                    d1, d2 = 0, 0
+                    for i in range(k):
+                        d1 += grid[r+i][c+i]
+                        d2 += grid[r+i][c+k-1-i]
+                            
+                    if d1 == target and d2 == target:
+                        return k
+        return 1
