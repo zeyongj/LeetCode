8a09d1e7198f97d53705b9e3fd4243c71327ada6
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2026-02-26 00:55:52 -0800

    Time: 0 ms (100%), Space: 19.2 MB (85.89%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
@@ -1,32 +1,13 @@
 class Solution:
-    def divide_by_two(self, s):
-        s.pop()
-
-    def add_one(self, s):
-        i = len(s) - 1
-
-        # Iterating while the character is 1 and changing to 0
-        while i >= 0 and s[i] != "0":
-            s[i] = "0"
-            i -= 1
-
-        if i < 0:
-            s.insert(0, "1")
-        else:
-            s[i] = "1"
-
     def numSteps(self, s: str) -> int:
-        s = list(s)
-        operations = 0
-
-        while len(s) > 1:
-            N = len(s)
-
-            if s[N - 1] == "0":
-                self.divide_by_two(s)
+        num = int(s, 2)
+        steps = 0
+        
+        while num != 1:
+            if num % 2 == 0:
+                num //= 2
             else:
-                self.add_one(s)
-
-            operations += 1
-
-        return operations
+                num += 1
+            steps += 1
+        
+        return steps
